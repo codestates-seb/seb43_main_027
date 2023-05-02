300fecdc68da4b0af1ee18eda253630ab3edc8fe
yeonssu <dustn7197@naver.com>
2023-05-02 14:21:28 +0900

    init : create project

---
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/InddybuddyApplication.java
@@ -0,0 +1,13 @@
+package codejejus.inddybuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InddybuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InddybuddyApplication.class, args);
+	}
+
+}
---
+++ server/inddybuddy/src/test/java/codejejus/inddybuddy/InddybuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package codejejus.inddybuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InddybuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
