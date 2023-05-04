2a3fd460838d3d1bd54952e3cb2f8a1b8cf50410
zqwz <zqwz@naver.com>
2023-05-04 16:52:15 +0900

    feat : 회원 팔로우, 메세지 entity 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/message/Message.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/message/Message.java
@@ -1,4 +1,31 @@
 package codejejus.inddybuddy.message;
 
-public class Message {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import codejejus.inddybuddy.global.audit.Timestamped;
+import codejejus.inddybuddy.member.Member;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Message extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long messageId;
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String content;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member sender;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member receiver;
 }
