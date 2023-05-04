e5530a77753fe326e38ce04c83558dea3392e79e
zqwz <zqwz@naver.com>
2023-05-04 22:00:09 +0900

    feat : 회원 principal 구현

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/member/MemberRepository.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/member/MemberRepository.java
@@ -3,5 +3,9 @@ package codejejus.inddybuddy.member;
 import codejejus.inddybuddy.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String username);
 }
