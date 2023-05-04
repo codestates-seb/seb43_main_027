422ca77bf64f32b959848689c6e1511901214343
zqwz <zqwz@naver.com>
2023-05-04 16:38:03 +0900

    feat : entity 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/follow/FollowGame.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/follow/FollowGame.java
@@ -1,4 +1,28 @@
 package codejejus.inddybuddy.follow;
 
-public class FollowGame {
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import codejejus.inddybuddy.global.audit.Timestamped;
+import codejejus.inddybuddy.member.Member;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class FollowGame extends Timestamped {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long followId;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "member_id")
+	private Member member;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "game_id")
+	private Game game;
 }
