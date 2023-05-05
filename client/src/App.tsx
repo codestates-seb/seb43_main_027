7a5ff1e86502f4e8f06829600d8bee33435f4a6f
crowwan <crowwan@naver.com>
2023-05-05 22:09:53 +0900

    init: 프로젝트 초기 세팅

---
+++ client/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>test</header>
+    </div>
+  );
+}
+
+export default App;
