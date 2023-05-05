58d002cc14b9b2f1d570a40c81986b6a1c83ce60
zqwz <zqwz@naver.com>
2023-05-05 17:45:39 +0900

    feat : 비밀번호 암호화, 권한 부여, 중복 이메일 방지 기능 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/global/config/SecurityConfig.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/global/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -41,6 +43,11 @@ public class SecurityConfig {
                 .anyRequest().permitAll();
 
         return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
     public class CustomFilterConfigurer extends AbstractHttpConfigurer<CustomFilterConfigurer, HttpSecurity> {
--- server/inddybuddy/src/main/java/codejejus/inddybuddy/global/exception/ExceptionCode.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/global/exception/ExceptionCode.java
@@ -7,7 +7,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ExceptionCode {
 
-    MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다.");
+    MEMBER_NOT_FOUND(404, "회원을 찾을 수 없습니다."),
+    MEMBER_EMAIL_EXIST(409, "이미 사용중인 이메일 입니다.");
     private final int code;
     private final String message;
 }
