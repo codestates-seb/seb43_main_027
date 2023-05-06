54aa6cd431ad1c3c6df6443e49b477b6b0585284
crowwan <crowwan@naver.com>
2023-05-06 18:36:08 +0900

    :recycle: refactor: 컴포넌트 함수 선언문에서 화살표 함수로 변경

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>test</header>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
