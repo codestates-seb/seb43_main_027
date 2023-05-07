2d2a17bd2b3316708d5ce62e3d098577e092403e
yeonssu <dustn7197@naver.com>
2023-05-07 17:59:52 +0900

    feat : 특정 Category 에 해당하는 모든 Game 조회

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/category/CategoryService.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/category/CategoryService.java
@@ -1,6 +1,11 @@
 package codejejus.inddybuddy.category;
 
+import codejejus.inddybuddy.game.Game;
+import codejejus.inddybuddy.game.GameDto;
+import codejejus.inddybuddy.game.GameMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +16,7 @@ public class CategoryService {
 
     private final CategoryRepository categoryRepository;
     private final CategoryMapper categoryMapper;
+    private final GameMapper gameMapper;
 
     public CategoryDto.Response createCategory(CategoryDto.Post postDto) {
         Category category = categoryMapper.postToEntity(postDto);
@@ -21,5 +27,10 @@ public class CategoryService {
     public List<CategoryDto.Response> getAllCategories() {
         List<Category> allCategories = categoryRepository.findAll();
         return categoryMapper.entityListToResponseList(allCategories);
+    }
+
+    public Page<GameDto.Response> findGamesByCategory(long categoryId, Pageable pageable) {
+        Page<Game> gamePage = categoryRepository.findAllByCategoryId(categoryId, pageable);
+        return gameMapper.entityPageToResponsePage(gamePage);
     }
 }
