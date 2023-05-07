18a57753eca5b6dd99f705075ed471e28ed47573
yeonssu <dustn7197@naver.com>
2023-05-07 16:51:50 +0900

    refactor : Game, Category Response 형식 수정

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/category/CategoryController.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/category/CategoryController.java
@@ -2,7 +2,6 @@ package codejejus.inddybuddy.category;
 
 import codejejus.inddybuddy.global.dto.SingleResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
