f445e64f3a740d190594c2c388deeafb8737811d
crowwan <crowwan@naver.com>
2023-05-08 14:52:47 +0900

    :sparkles: feat: 헤더 컴포넌트 확인을 위한 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from './styles/globalStyles';
+import Header from './layouts/Header/Header';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Header />
     </>
   );
 };
