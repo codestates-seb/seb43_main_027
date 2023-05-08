ab774268f41a3785b342f9739fa8c4584ecf1362
UseonJ <naul630@gmail.com>
2023-05-08 18:30:13 +0900

    :construction: wip: 인풋타입 임시지정

---
+++ client/src/types/componentsTypes.ts
@@ -0,0 +1,23 @@
+export type ButtonType = {
+  fontSize?: string,
+  fontWeight?: string,
+  bg?: string,
+  fontColor?: string,
+  margin?: string,
+  padding?: string,  
+  radius?: string,
+  border?: string,
+  hoverBg?: string,
+}
+
+export type InputType = {
+  fontSize?: string,
+  fontWeight?: string,
+  bg?: string,
+  fontColor?: string,
+  margin?: string,
+  padding?: string,  
+  radius?: string,
+  border?: string,
+  hoverBg?: string,
+}
