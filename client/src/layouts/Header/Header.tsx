c926aaf0bbc42b49d927b2a38c070a99ae669c18
crowwan <crowwan@naver.com>
2023-05-08 16:02:53 +0900

    :sparkles: feat: 비 로그인 시 헤더 UI작업 완료

---
+++ client/src/layouts/Header/Header.tsx
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+import MenuBtn from './MenuBtn';
+import Logo from './Logo';
+import SearchBar from './SearchBar';
+import HeaderBtnContainer from './HeaderBtnContainer';
+
+const Header = () => {
+  return (
+    <StyledContainer>
+      <MenuBtn />
+      <StyledResponsiveContainer>
+        <Logo />
+        <SearchBar />
+      </StyledResponsiveContainer>
+      <HeaderBtnContainer />
+    </StyledContainer>
+  );
+};
+
+export default Header;
+
+const StyledContainer = styled.div`
+  display: flex;
+  position: fixed;
+  gap: 0.8rem;
+  top: 0;
+  align-items: center;
+  background-color: var(--cyan-light-100);
+  height: 50px;
+  width: 100vw;
+  padding: 0 2rem;
+`;
+const StyledResponsiveContainer = styled.div`
+  flex: 1 0 0;
+  display: flex;
+  justify-content: space-between;
+  gap: 0.8rem;
+  align-items: center;
+`;
