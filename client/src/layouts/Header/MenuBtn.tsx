f8b7d46e347be788f9023b7171a58991c6b680dc
crowwan <crowwan@naver.com>
2023-05-08 15:58:34 +0900

    :sparkles: feat: 헤더 모바일 디스플레이 메뉴 버튼 UI 작업 완료

---
+++ client/src/layouts/Header/MenuBtn.tsx
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+const MenuBtn = () => {
+  return (
+    <StyledMenuButton>
+      <div />
+      <div />
+      <div />
+    </StyledMenuButton>
+  );
+};
+
+const StyledMenuButton = styled.div`
+  cursor: pointer;
+  position: relative;
+  display: none;
+  flex-direction: column;
+  gap: 4px;
+  & div {
+    width: 25px;
+    height: 3px;
+    background-color: var(--cyan-dark-500);
+  }
+  @media screen and (max-width: 768px) {
+    display: flex;
+  }
+`;
+
+export default MenuBtn;
