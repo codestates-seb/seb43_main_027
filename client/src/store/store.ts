342f189315a208f905fbe5423d87ccf1011a3163
crowwan <crowwan@naver.com>
2023-05-08 17:19:12 +0900

    :sparkles: feat: 스토어에 유저 슬라이스 추가

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userSlice from '../slice/userSlice';
 
 const store = configureStore({
-  reducer: {}
+  reducer: {
+    user: userSlice
+  }
 });
+export type RootState = ReturnType<typeof store.getState>;
 export default store;
