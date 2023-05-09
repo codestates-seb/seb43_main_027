43208a81a301b8535803a2f24c26a796bd9f34bd
crowwan <crowwan@naver.com>
2023-05-09 15:44:50 +0900

    :sparkles: feat: 모바일 디스플레이 네비게이션바 상태 추가
    모바일 화면에서 네비게이션바를 메뉴버튼으로 조작하기 위한 상태 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GlobalStyle from './styles/globalStyles';
 import Header from './layouts/Header/Header';
 import Nav from './layouts/Nav/Nav';
 
 const App = () => {
+  const [show, setShow] = useState(false);
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <Nav />
+      <Header setShow={setShow} show={show} />
+      <Nav show={show} />
     </>
   );
 };
