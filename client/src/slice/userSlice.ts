818fdc672f32dfcf5e114e77c4d1d1f93b7f2698
crowwan <crowwan@naver.com>
2023-05-09 15:52:57 +0900

    :sparkles: feat: 유저 기본 상태 변경

--- client/src/slice/userSlice.ts
+++ client/src/slice/userSlice.ts
@@ -1,14 +1,25 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { User } from '../types/dataTypes';
+
+const initialState: User = {
+  memberId: -1,
+  email: '',
+  username: '',
+  memberStatus: '',
+  imageUrl: '',
+  createdAt: '',
+  updatedAt: ''
+};
 
 const userSlice = createSlice({
   name: 'user',
-  initialState: '',
+  initialState: { ...initialState },
   reducers: {
     setUser(state, action) {
       return action.payload;
     },
     clearUser() {
-      return '';
+      return initialState;
     }
   }
 });
