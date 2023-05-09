f2e6098cb945bae536fb5f69ab94a5ace6cce1ed
crowwan <crowwan@naver.com>
2023-05-09 17:55:28 +0900

    :sparkles: feat: 게시글 공통 컴포넌트 구현 완료

---
+++ client/src/components/common/PostItem.tsx
@@ -0,0 +1,100 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
+import Badge from '../ui/Badge';
+
+const PostItem = () => {
+  return (
+    <StyledContainer>
+      <div>
+        <PostTitle />
+        <PostInfo />
+      </div>
+      <StyledSubContent>
+        <PostSubContent />
+      </StyledSubContent>
+    </StyledContainer>
+  );
+};
+
+const PostInfo = () => {
+  return (
+    <StyledPostInfoContainer>
+      <span>작성자: username</span>
+      <span>작성일: 1분 전</span>
+      <span>추천 수: 0</span>
+    </StyledPostInfoContainer>
+  );
+};
+
+const PostTitle = () => {
+  return (
+    <StyledTitleContainer>
+      <StyledTitle>title</StyledTitle>
+      <Badge text='모집 중' />
+    </StyledTitleContainer>
+  );
+};
+
+const PostSubContent = () => {
+  const [marked, setMarked] = useState(false);
+
+  const invertMarked = () => {
+    setMarked((prev) => !prev);
+  };
+  return (
+    <StyledPostSubContentContainer>
+      <span>댓글 : 0</span>
+      {marked ? (
+        <AiFillStar
+          color={'var(--cyan-dark-500)'}
+          size={20}
+          onClick={invertMarked}
+        />
+      ) : (
+        <AiOutlineStar
+          color={'var(--cyan-dark-500)'}
+          size={20}
+          onClick={invertMarked}
+        />
+      )}
+    </StyledPostSubContentContainer>
+  );
+};
+export default PostItem;
+
+const StyledContainer = styled.div`
+  display: flex;
+  padding: 1rem 2rem;
+  background-color: #fff;
+  width: 100%;
+  border-bottom: 1px solid var(--cyan-dark-500);
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledSubContent = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+const StyledTitleContainer = styled.div`
+  display: flex;
+  gap: 2rem;
+`;
+const StyledTitle = styled.h3`
+  font-weight: bold;
+  font-size: 2rem;
+`;
+const StyledPostInfoContainer = styled.div`
+  display: flex;
+  color: rgba(0, 0, 0, 0.45);
+  gap: 2rem;
+  font-size: 1.2rem;
+  margin-top: 1rem;
+`;
+const StyledPostSubContentContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
