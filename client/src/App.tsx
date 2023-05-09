35fac703921d7eb71847c6cd1c0b1515a6a9426d
crowwan <crowwan@naver.com>
2023-05-09 11:28:20 +0900

    :sparkles: feat: 네비게이션바 컴포넌트 확인을 위한 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import GlobalStyle from './styles/globalStyles';
 import Header from './layouts/Header/Header';
+import Nav from './layouts/Nav/Nav';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
       <Header />
+      <Nav />
     </>
   );
 };
