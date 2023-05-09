7213e4ea938e0b38271bc2ad99dec352ddfe1a21
UseonJ <naul630@gmail.com>
2023-05-09 14:13:54 +0900

    :construction: wip: 작업중

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import GlobalStyle from './styles/GlobalStyles';
+import SignUp from './pages/SignUp/SignUp';
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <SignUp />
     </>
   );
 };
