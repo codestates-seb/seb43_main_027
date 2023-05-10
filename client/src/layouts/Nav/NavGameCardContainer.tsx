bd91c69450778b7056f38552637f6fc130d54a26
crowwan <crowwan@naver.com>
2023-05-10 23:00:30 +0900

    :sparkles: feat: ui 확인을 위한 게임 더미데이터 추가

--- client/src/layouts/Nav/NavGameCardContainer.tsx
+++ client/src/layouts/Nav/NavGameCardContainer.tsx
@@ -1,10 +1,27 @@
 import styled from 'styled-components';
 import GameCard from '../../components/common/GameCard';
 
+import logo from '../../asset/logo.png';
+
+import { GameType } from '../../types/dataTypes';
+
+const dummy: GameType = {
+  gameId: 1,
+  mainImgUrl: logo,
+  downloadUrl: 'test',
+  gameName: 'testtestsetset',
+  categories: [
+    {
+      categoryId: 1,
+      categoryName: 'RPG'
+    }
+  ]
+};
+
 const NavGameCardContainer = () => {
   return (
     <StyledContainer>
-      <GameCard />
+      <GameCard {...dummy} />
     </StyledContainer>
   );
 };
