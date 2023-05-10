e792cd4b565f48e037ddc26994957df93de25af7
crowwan <crowwan@naver.com>
2023-05-10 22:59:17 +0900

    :sparkles: feat: 더미 게임 채널 카드 제거 및 게임 채널 상태 추가

--- client/src/layouts/Home/GameContainer.tsx
+++ client/src/layouts/Home/GameContainer.tsx
@@ -1,18 +1,31 @@
+import { useState } from 'react';
 import styled from 'styled-components';
+
+import { Empty } from 'antd';
 import FilterBar from './FilterBar';
 import HomeGameCard from './HomeGameCard';
 
+import { GameType } from '../../types/dataTypes';
+
 const GameContainer = () => {
+  const [games, setGames] = useState<GameType[]>([]);
   return (
     <StyledWrapper>
       <StyledContainer>
-        <FilterBar />
+        <FilterBar setGames={setGames} />
         <StyledCardContainer>
-          <HomeGameCard />
-          <HomeGameCard />
-          <HomeGameCard />
-          <HomeGameCard />
-          <HomeGameCard />
+          {games.length === 0 && (
+            <Empty
+              description={
+                <StyledNotFoundText>
+                  게임을 찾을 수 없습니다.
+                </StyledNotFoundText>
+              }
+            />
+          )}
+          {games.map((game) => (
+            <HomeGameCard key={game.gameId} {...game} />
+          ))}
         </StyledCardContainer>
       </StyledContainer>
     </StyledWrapper>
@@ -28,6 +41,7 @@ const StyledWrapper = styled.div`
   justify-content: center;
   padding: 5rem 0;
 `;
+
 const StyledContainer = styled.div`
   width: 100%;
 
@@ -39,8 +53,14 @@ const StyledContainer = styled.div`
 const StyledCardContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
   gap: 3rem 5%;
   @media screen and (min-width: 1200px) {
     gap: 3rem 4%;
   }
+`;
+
+const StyledNotFoundText = styled.div`
+  margin-top: 3rem;
 `;
