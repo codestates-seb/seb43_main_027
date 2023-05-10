ca973a5079dd85f80407cfde03d5543af8a3408e
crowwan <crowwan@naver.com>
2023-05-10 10:50:55 +0900

    :sparkles: feat: 푸터 프롭스 타입 추가

--- client/src/types/propsTypes.ts
+++ client/src/types/propsTypes.ts
@@ -2,3 +2,8 @@ export type NavStateType = {
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
   show: boolean;
 };
+export type MemberType = {
+  src: string;
+  name: string;
+  type: 'Front-End' | 'Back-End';
+};
