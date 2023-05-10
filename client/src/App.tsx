f7e1830f626da3663cbc63530f894cd69486f593
crowwan <crowwan@naver.com>
2023-05-10 10:50:40 +0900

    :sparkles: feat: 푸터 확인을 위한 컴포넌트 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import GlobalStyle from './styles/globalStyles';
 import Header from './layouts/Header/Header';
 import Nav from './layouts/Nav/Nav';
+import Footer from './layouts/Footer/Footer';
 
 const App = () => {
   const [show, setShow] = useState(false);
@@ -10,6 +11,7 @@ const App = () => {
       <GlobalStyle />
       <Header setShow={setShow} show={show} />
       <Nav show={show} setShow={setShow} />
+      <Footer />
     </>
   );
 };
