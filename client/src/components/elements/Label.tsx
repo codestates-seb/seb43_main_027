1c05e8028f49b1c959b4879ef7c69db262cfa8dd
UseonJ <naul630@gmail.com>
2023-05-10 17:12:03 +0900

    :truck: feat: input에 붙는 label 엘리먼트 생성

---
+++ client/src/components/elements/Label.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { LabelType } from '../../types/componentsTypes';
+
+export const StyledLabel = styled.label<LabelType>`
+  font-size: 1.6rem;
+  margin: 1rem 1rem;
+  padding: 0.5rem 0.5rem;
+  border-radius: 4px;
+`;
