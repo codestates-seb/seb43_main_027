b5de46373a3d4e8305ae3617a166c134b4455060
UseonJ <naul630@gmail.com>
2023-05-11 02:06:31 +0900

    :construction: wip: 테스트 위한 루트설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,13 +3,16 @@ import GlobalStyle from './styles/globalStyles';
 import { Route, Routes } from 'react-router-dom';
 import Template from './pages/Template';
 import CategoryGames from './pages/CategoryGames';
+import SignUp from './pages/SignUp/SignUp';
 
 const App = () => {
   return (
     <>
+      <GlobalStyle />
       <Routes>
         <Route path='/' element={<Template />}>
           <Route path='/category/:id' element={<CategoryGames />} />
+          <Route path='/signup' element={<SignUp />} />
         </Route>
       </Routes>
     </>
