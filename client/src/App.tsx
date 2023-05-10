df5ede79cb473942916f0b72340f8882575bffd5
crowwan <crowwan@naver.com>
2023-05-10 17:31:26 +0900

    :sparkles: feat: 홈 페이지 라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import GlobalStyle from './styles/globalStyles';
 import { Route, Routes } from 'react-router-dom';
 import Template from './pages/Template';
 import CategoryGames from './pages/CategoryGames';
+import Home from './pages/Home';
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <GlobalStyle />
       <Routes>
         <Route path='/' element={<Template />}>
+          <Route path='/' element={<Home />} />
           <Route path='/category/:id' element={<CategoryGames />} />
         </Route>
       </Routes>
