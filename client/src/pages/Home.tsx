5fbe90ba7953bf6c26ebef6435212f27e73d3984
crowwan <crowwan@naver.com>
2023-05-10 23:01:05 +0900

    :art: design: 컴포넌트 최소 높이 설정 100vh - 50px

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+
 import CategoryContainer from '../layouts/Home/CategoryContainer';
 import GameContainer from '../layouts/Home/GameContainer';
 
@@ -14,6 +15,6 @@ const Home = () => {
 export default Home;
 const StyleContain = styled.div`
   width: 100%;
-  min-height: 100%;
+  min-height: calc(100vh - 50px);
   background-color: var(--title-bg);
 `;
