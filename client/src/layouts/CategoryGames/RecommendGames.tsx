e17a6ee75ae77071e5df8cc56111db773531890b
crowwan <crowwan@naver.com>
2023-05-10 13:40:36 +0900

    :lipstick: style: 세미콜론 추가

--- client/src/layouts/CategoryGames/RecommendGames.tsx
+++ client/src/layouts/CategoryGames/RecommendGames.tsx
@@ -12,20 +12,42 @@ interface Swiper {
 type Slide = {
   key: number;
   image: string;
-}
+};
 
 const slides: Slide[] = [
-  { key: 0, image: 'https://m.gjcdn.net/game-thumbnail/1000/435648-bcpdubiq-v4.webp' },
-  { key: 1, image: 'https://m.gjcdn.net/game-thumbnail/1000/164227-f2fvqeih-v4.webp' },
-  { key: 2, image: 'https://m.gjcdn.net/game-thumbnail/1000/348021-crop10_0_598_331-w5vedzau-v4.webp' },
-  { key: 3, image: 'https://m.gjcdn.net/game-thumbnail/1000/352743-an4gjy3v-v4.webp' },
-  { key: 4, image: 'https://m.gjcdn.net/game-thumbnail/1000/415163-pm6dqtkg-v4.webp' },
-  { key: 5, image: 'https://m.gjcdn.net/game-thumbnail/1000/370565-crop0_3_616_350-hhcbzd8r-v4.webp' },
-  { key: 6, image: 'https://m.gjcdn.net/game-thumbnail/1000/651214-rpywxctu-v4.webp' },
+  {
+    key: 0,
+    image: 'https://m.gjcdn.net/game-thumbnail/1000/435648-bcpdubiq-v4.webp'
+  },
+  {
+    key: 1,
+    image: 'https://m.gjcdn.net/game-thumbnail/1000/164227-f2fvqeih-v4.webp'
+  },
+  {
+    key: 2,
+    image:
+      'https://m.gjcdn.net/game-thumbnail/1000/348021-crop10_0_598_331-w5vedzau-v4.webp'
+  },
+  {
+    key: 3,
+    image: 'https://m.gjcdn.net/game-thumbnail/1000/352743-an4gjy3v-v4.webp'
+  },
+  {
+    key: 4,
+    image: 'https://m.gjcdn.net/game-thumbnail/1000/415163-pm6dqtkg-v4.webp'
+  },
+  {
+    key: 5,
+    image:
+      'https://m.gjcdn.net/game-thumbnail/1000/370565-crop0_3_616_350-hhcbzd8r-v4.webp'
+  },
+  {
+    key: 6,
+    image: 'https://m.gjcdn.net/game-thumbnail/1000/651214-rpywxctu-v4.webp'
+  }
 ];
 
-const RecommedGames = ()  => {
-
+const RecommedGames = () => {
   const swiperRef = useRef<Swiper>(null);
 
   useEffect(() => {
@@ -52,22 +74,19 @@ const RecommedGames = ()  => {
           rotate: 0,
           stretch: 0,
           depth: 100,
-          modifier: 2.5,
+          modifier: 2.5
         }}
         modules={[EffectCoverflow]}
       >
-
-    {
-      slides.map((slide) => (
-        <StyledSwiperSlide key={slide.key}>
-          <StyledSwiperSlideImg src={slide.image} alt='slide_image' />
-        </StyledSwiperSlide>
-      ))
-    }
+        {slides.map((slide) => (
+          <StyledSwiperSlide key={slide.key}>
+            <StyledSwiperSlideImg src={slide.image} alt='slide_image' />
+          </StyledSwiperSlide>
+        ))}
       </StyledSwiperContainer>
     </StyledContainer>
   );
-}
+};
 
 export default React.memo(RecommedGames);
 
@@ -78,6 +97,10 @@ const StyledContainer = styled.div`
   max-width: 132rem;
   padding: 1rem 1rem;
   margin: 0 auto;
+
+  .swiper {
+    z-index: 0;
+  }
 `;
 
 const StyledSwiperContainer = styled(Swiper)`
@@ -105,8 +128,7 @@ const StyledSwiperSlideImg = styled.img`
   border-radius: 2rem;
   object-fit: cover;
   cursor: pointer;
-  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1),
-    0px 6px 6px rgba(0, 0, 0, 0.1);
+  box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.1), 0px 6px 6px rgba(0, 0, 0, 0.1);
   filter: drop-shadow(0px 10px 10px rgba(0, 0, 0, 0.1));
   @media (max-width: 650px) {
     width: 28rem !important;
