a87e6405a4b727667626f464088370a97d704ae2
crowwan <crowwan@naver.com>
2023-05-11 14:06:48 +0900

    :sparkles: feat: SignUp 컴포넌트 추가 및 라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Template from './pages/Template';
 import CategoryGames from './pages/CategoryGames';
 import Home from './pages/Home';
+import SignUp from './pages/SignUp/SignUp';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='/' element={<Template />}>
           <Route path='/' element={<Home />} />
           <Route path='/category/:id' element={<CategoryGames />} />
+          <Route path='/signup' element={<SignUp />} />
         </Route>
       </Routes>
     </>
