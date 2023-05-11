683d57f1e0057fabba39dcd63ce1a1c0825af5f6
zqwz <zqwz@naver.com>
2023-05-11 16:48:52 +0900

    feat : oauth redirect 기능 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/global/oauth/handler/OAuth2LoginSuccessHandler.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/global/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -9,10 +9,12 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -21,16 +23,29 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtTokenProvider jwtTokenProvider;
 
+    private static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    return Optional.of(cookie);
+                }
+            }
+        }
+        return Optional.empty();
+    }
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
                                         HttpServletResponse response,
                                         Authentication authentication) throws IOException {
         log.info("OAuth login 성공");
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-        loginSuccess(response, oAuth2User);
+        loginSuccess(response, request, oAuth2User);
     }
 
-    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+    private void loginSuccess(HttpServletResponse response, HttpServletRequest request, CustomOAuth2User oAuth2User) throws IOException {
         String accessToken = jwtTokenProvider.generateAccessToken(oAuth2User.getEmail(), oAuth2User.getRoles());
         String refreshToken = jwtTokenProvider.generateRefreshToken();
         response.setHeader("Authorization", "Bearer " + accessToken);
@@ -38,8 +53,9 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         log.info("accessToken : {}", accessToken);
         log.info("refreshToken : {}", refreshToken);
 
+        Optional<String> redirectUri = getCookie(request, "redirect_uri").map(Cookie::getValue);
         jwtTokenProvider.sendAccessAndRefreshToken(response, accessToken, refreshToken);
-        response.sendRedirect(createURI());
+        response.sendRedirect(redirectUri.orElseGet(this::createURI));
     }
 
     private String createURI() {
