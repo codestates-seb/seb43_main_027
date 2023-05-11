934bc63cca10915761d9fd780248f9b8480e7c7d
zqwz <zqwz@naver.com>
2023-05-11 17:10:28 +0900

    refactor : oauth redirect 주소 변경

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/global/oauth/handler/OAuth2LoginSuccessHandler.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/global/oauth/handler/OAuth2LoginSuccessHandler.java
@@ -9,10 +9,12 @@ import org.springframework.security.web.authentication.AuthenticationSuccessHand
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -20,6 +22,19 @@ import java.nio.charset.StandardCharsets;
 public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
 
     private final JwtTokenProvider jwtTokenProvider;
+
+    private static Optional<Cookie> getCookie(HttpServletRequest request, String name) {
+        Cookie[] cookies = request.getCookies();
+
+        if (cookies != null && cookies.length > 0) {
+            for (Cookie cookie : cookies) {
+                if (name.equals(cookie.getName())) {
+                    return Optional.of(cookie);
+                }
+            }
+        }
+        return Optional.empty();
+    }
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request,
@@ -38,6 +53,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         log.info("accessToken : {}", accessToken);
         log.info("refreshToken : {}", refreshToken);
         jwtTokenProvider.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+        getCookie(request, "redirect_uri");
         response.sendRedirect(createURI());
     }
 
