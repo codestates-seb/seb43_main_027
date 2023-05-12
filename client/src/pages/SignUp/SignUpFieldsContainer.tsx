2fa892627cb596cd79565869174e4b94d8a62fe0
UseonJ <naul630@gmail.com>
2023-05-12 11:10:30 +0900

    :sparkles: feat: 유효성 메세지 추가

--- client/src/pages/SignUp/SignUpFieldsContainer.tsx
+++ client/src/pages/SignUp/SignUpFieldsContainer.tsx
@@ -15,19 +15,19 @@ const SignUpFieldsContainer = ({
         placeholder='ex)인디벗'
         title='닉네임'
         useInput={inputUserName}
-        validmessage={validmessage}
+        validmessage='한글/영문 2-10자로 작성해주세요.'
       ></SignUpFieldsInputContainer>
       <SignUpFieldsInputContainer
         placeholder='ex)inddy@gmail.com'
         title='아이디'
         useInput={inputEmail}
-        validmessage={validmessage}
+        validmessage='유효한 이메일 형식이 아닙니다.'
       ></SignUpFieldsInputContainer>
       <SignUpFieldsInputContainer
         placeholder='8자리 이상'
         title='비밀번호'
         useInput={inputPassWord}
-        validmessage={validmessage}
+        validmessage='비밀번호는 영문 대/소문자,숫자,특수문자의 조합으로 8-16자이어야 합니다.'
       ></SignUpFieldsInputContainer>
     </StyledSignUpFieldsContainer>
   );
