e78bed4b1f4a4496457d82d43374b39dd434f44c
UseonJ <naul630@gmail.com>
2023-05-12 11:09:30 +0900

    :sparkles: feat: validitySlice 추가

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userSlice from '../slice/userSlice';
+import validitySlice from '../slice/validitySlice';
 
 const store = configureStore({
   reducer: {
-    user: userSlice
+    user: userSlice,
+    validity: validitySlice,
   }
 });
 export type RootState = ReturnType<typeof store.getState>;
