46462ab13bbdcf05754488ea0c0ca8ab95ac4050
UseonJ <naul630@gmail.com>
2023-05-12 12:22:46 +0900

    :label: type: validity type 추가

--- client/src/types/costomHooksTypes.ts
+++ client/src/types/costomHooksTypes.ts
@@ -8,5 +8,7 @@ export interface useInputReturn {
   value: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   setValue: React.Dispatch<React.SetStateAction<string>>;
+  validity: boolean;
 };
 
+export type useValidityType = 'username' | 'email' | 'password';
