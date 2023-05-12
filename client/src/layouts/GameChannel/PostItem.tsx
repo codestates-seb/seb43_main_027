a48dffc3040af487b986c60221dac6427890fd22
한수지 <fatholic5@gmail.com>
2023-05-12 10:16:15 +0900

    :recycle: refactor: 게시글 아이템 데이터,ui 변경

--- client/src/layouts/GameChannel/PostItem.tsx
+++ client/src/layouts/GameChannel/PostItem.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import CategoryTag from '../../components/common/CategoryTag';
+import { StarTwoTone } from '@ant-design/icons';
 
 const PostItem = ()  => {
+
+  const [ isMarked, setMarked ] = useState(false);
+
+  const handleMark = () => {
+    setMarked((prev) => !prev)
+  }
+
+  // 필터링 데이터패칭 해서 받아와야됨, 글자수 제한 ... 
+
   return (
     <StyledWrapper>
       <StyledContent>
         <StyledFlexRow>
-          제목
-          태그
+          <StyledTitle>
+            제목 엄청나게 긴 제목제목 엄청나게 긴 제목 ...
+          </StyledTitle>
+          <CategoryTag index={0} categoryName='모집'/>
         </StyledFlexRow>
         <StyledFlexRow>
-          작성자
-          작성일
-          추천수
-          조회수
+          <StyledInfo>
+            <p>작성자: </p>
+            <p>작성일: </p>
+            <p>추천수: </p>
+            <p>조회수: </p>      
+          </StyledInfo>
         </StyledFlexRow>
       </StyledContent>
       <StyledFlexRow>
-        댓글: 10
-        북마크
+      <StyledInfo>
+        <p>댓글: 0</p>
+        <StarTwoTone
+          onClick={handleMark}
+          twoToneColor={ isMarked ? '#13A8A8'  : '#b4b4b4' }
+          style={{ fontSize: '20px' }}
+        />
+      </StyledInfo>
       </StyledFlexRow>
     </StyledWrapper>
   );
@@ -50,3 +71,22 @@ const StyledFlexRow = styled.div`
   display: flex;
   flex-direction: row;
 `;
+
+const StyledTitle = styled.h3`
+  font-size: 16px;
+  font-weight: 700;
+  margin-right: 20px;
+  word-break: keep-all;
+  overflow-wrap: break-word;
+`;
+
+const StyledInfo = styled.p`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-content: center;
+  gap: 10px;
+  margin-top: 10px;
+  font-size: 14px;
+  color: var(--default-text-color);
+`
