1e8fc4719b6317b528aba37d00800307139e1692
한수지 <fatholic5@gmail.com>
2023-05-13 19:34:03 +0900

    :sparkles: feat: 게시글 리스트 필터링 기능 추가 사용자가 선택한 태그와 탭의 상태값을 전달받아 조건에 맞게 리스트를 새로 생성함, 최신순은 최신 작성된 날짜순으로 정렬, 인기순은 추천수가 많은수로 정렬, 조회순은 조회수가 많은수로 정렬, 태그를 선택하면 해당 태그의 리스트만 조회할 수 있도록 설정함(기본값은 전체) 조회할 데이터가 없으면 데이터 없음을 출력함

--- client/src/layouts/GameChannel/PostList.tsx
+++ client/src/layouts/GameChannel/PostList.tsx
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import PostItem from './PostItem';
+import { dummyPostList } from '../../data/dummyPostList';
 
-const PostList = ()  => {
+interface Props {
+  gameId?: string;
+  isSelectTab: string;
+  isSelectTag: string;
+}
+
+const PostList: React.FC<Props> = ({ gameId, isSelectTag ,isSelectTab }) => {
+  const [filteredPosts, setFilteredPosts] = useState(dummyPostList.post);
+
+  useEffect(() => {
+    const postData = dummyPostList.post;
+  
+    switch (isSelectTab) {
+      case '최신순':
+        setFilteredPosts([...postData].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()));
+        break;
+      case '인기순':
+        setFilteredPosts([...postData].sort((a, b) => b.likeCount - a.likeCount));
+        break;
+      case '조회순':
+        setFilteredPosts([...postData].sort((a, b) => b.view - a.view));
+        break;
+      default:
+        setFilteredPosts(postData);
+        break;
+    }
+
+    if (isSelectTag !== '전체') {
+      setFilteredPosts((prev) => prev.filter((post) => post.tag === isSelectTag));
+    }
+  }, [isSelectTab, isSelectTag]);
+
+  console.log(filteredPosts);
+
   return (
     <PostListWrapper>
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
-      <PostItem />
+      {
+        filteredPosts.length > 0 ? 
+        filteredPosts.map((post) => (
+          <PostItem
+            key={post.postId}
+            postId={post.postId}
+            title={post.title}
+            tag={post.tag}
+            createdAt={post.createdAt}
+            updatedAt={post.updatedAt}
+            view={post.view}
+            userName={post.userName}
+            memberStatus={post.memberStatus}
+            likeCount={post.likeCount}
+            commentCount={post.commentCount}
+          />
+        )) : 
+        <StyledEmptyItem>
+          작성된 게시글이 없습니다.
+        </StyledEmptyItem>
+      }
     </PostListWrapper>
-  )
-}
+  );
+};
 
 export default PostList;
 
@@ -30,4 +75,14 @@ const PostListWrapper = styled.div`
     padding: 0px 5px;
     max-height: 100%;
   }
+`;
+
+const StyledEmptyItem = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  font-size: 18px;
+  font-weight: 700;
+  color: var(--default-text-color);
 `;
