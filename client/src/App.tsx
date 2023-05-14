e0599ca55f9523c3931e587c6e43db28a25b0607
UseonJ <naul630@gmail.com>
2023-05-14 20:04:03 +0900

    :sparkles: feat: 게임등록페이지 경로추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Template from './pages/Template';
 import CategoryGames from './pages/CategoryGames';
 import SignUp from './pages/SignUp/SignUp';
+import GameRegister from './pages/GameRegister/GameRegister';
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path='/' element={<Template />}>
           <Route path='/category/:id' element={<CategoryGames />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/games' element={<GameRegister />} />
         </Route>
       </Routes>
     </>
