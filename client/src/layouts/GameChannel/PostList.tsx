5a82e524831ee0e75c5241daad0043af1f881314
한수지 <fatholic5@gmail.com>
2023-05-14 18:55:48 +0900

    :recycle: refactor: 로그인 상태에 따른 탭이벤트 추가 비로그인시 북마크, 내가쓴글 탭 클릭시 로그인 필요 메세지 출력, 로그인시에 북마크, 내가 쓴글 탭 클릭시 해당 데이터 조회 추후 api 연결 필요

--- client/src/layouts/GameChannel/PostList.tsx
+++ client/src/layouts/GameChannel/PostList.tsx
@@ -1,18 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 import Pagination from 'react-js-pagination';
 import PostItem from './PostItem';
-import { dummyPostList } from '../../data/dummyPostList';
+import { dummyPostList, dummyBookmarkList, dummyMyList } from '../../data/dummyPostList';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
 
 interface Props {
   gameId?: string;
   isSelectTab: string;
   isSelectTag: string;
-}
+};
 
 const PostList: React.FC<Props> = ({ gameId, isSelectTag ,isSelectTab }) => {
   const [filteredPosts, setFilteredPosts] = useState(dummyPostList.post);
   const [page, setPage] = useState(1);
+  const [userMessage, setUserMessage ] = useState('작성된 게시글이 없습니다.');
+  const memberId = useSelector((state: RootState) => state.user.memberId);
 
   const handlePageChange = (page: number) => {
     setPage(page);
@@ -20,7 +25,10 @@ const PostList: React.FC<Props> = ({ gameId, isSelectTag ,isSelectTab }) => {
 
   useEffect(() => {
     setPage(1);
+    setUserMessage('작성된 게시글이 없습니다.');
     const postData = dummyPostList.post;
+    const bookmarkData = dummyBookmarkList.post;
+    const myPostData = dummyMyList.post;
   
     switch (isSelectTab) {
       case '최신순':
@@ -32,6 +40,42 @@ const PostList: React.FC<Props> = ({ gameId, isSelectTag ,isSelectTab }) => {
       case '조회순':
         setFilteredPosts([...postData].sort((a, b) => b.view - a.view));
         break;
+      case '북마크 글':
+        if (memberId === -1) {
+          setFilteredPosts([]);
+          setUserMessage('로그인이 필요한 기능입니다.');
+        } else {
+          // 로그인 후 북마크한 글들을 가져오는 코드 작성
+          /* api 연결시 사용
+          axios.get(`${process.env.REACT_APP_API_URL}/api/members/${memberId}/bookmark`)
+            .then((response) => {
+              setFilteredPosts(response.data);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+          */
+          setFilteredPosts([...bookmarkData].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()));
+        }
+        break;
+      case '내가 쓴 글':
+        if (memberId === -1) {
+          setFilteredPosts([]);
+          setUserMessage('로그인이 필요한 기능입니다.');
+        } else {
+          // 로그인 후 내가 쓴 글들을 가져오는 코드 작성
+          /* api 연결시 사용
+          axios.get(`${process.env.REACT_APP_API_URL}/api/members/${memberId}/mypost`)
+            .then((response) => {
+              setFilteredPosts(response.data);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+          */
+          setFilteredPosts([...myPostData].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()));
+        }
+        break;
       default:
         setFilteredPosts(postData);
         break;
@@ -40,7 +84,7 @@ const PostList: React.FC<Props> = ({ gameId, isSelectTag ,isSelectTab }) => {
     if (isSelectTag !== '전체') {
       setFilteredPosts((prev) => prev.filter((post) => post.tag === isSelectTag));
     }
-  }, [isSelectTab, isSelectTag]);
+  }, [isSelectTab, isSelectTag, memberId]);
 
   const ITEMS_PER_PAGE = 10;
   const lastIndex = page * ITEMS_PER_PAGE;
@@ -67,7 +111,7 @@ const PostList: React.FC<Props> = ({ gameId, isSelectTag ,isSelectTab }) => {
           />
         )) :
         <StyledEmptyItem>
-          작성된 게시글이 없습니다.
+          {userMessage}
         </StyledEmptyItem>
       }
       <StyledPagination>
@@ -100,7 +144,7 @@ const StyledEmptyItem = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 50px;
+  padding: 50px 0px;
   font-size: 18px;
   font-weight: 700;
   color: var(--default-text-color);
