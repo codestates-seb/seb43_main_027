9728f15bc3c79eeabd776b270ae61e7162604a6b
yeonssu <dustn7197@naver.com>
2023-05-15 14:42:39 +0900

    refactor : Post delete 메서드 status 관련 삭제

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/post/PostRepository.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/post/PostRepository.java
@@ -3,16 +3,9 @@ package codejejus.inddybuddy.post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.Optional;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @Query("FROM Post  p where p.postStatus <> 'POST_DELEDTED' AND p.postId = :postId")
-    Optional<Post> findByIdNotDeleted(Long postId);
     Page<Post> findAll(Pageable pageable);
-    Page<Post> findAllByPostTag(Post.PostTag postTag, Pageable pageale);
+    Page<Post> findAllByPostTag(Post.PostTag postTag, Pageable pageable);
 }
-
-
