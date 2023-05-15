2070c0cc764fecd0badfe4c124452b9a7c24929c
한수지 <fatholic5@gmail.com>
2023-05-16 08:19:42 +0900

    :recycle: refactor: 카테고리페이지 사용하지 않는 모듈 제거

--- client/src/pages/CategoryGames.tsx
+++ client/src/pages/CategoryGames.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import GameList from '../layouts/CategoryGames/GameList';
 import FilterTap from '../components/common/FilterTap';
