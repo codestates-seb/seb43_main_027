5b283f19520e5378bb1e6202d427ea1ec825301e
crowwan <crowwan@naver.com>
2023-05-16 16:09:03 +0900

    :sparkles: feat: 이미지 미리보기 기능 컴포넌트 생성

---
+++ client/src/layouts/Posting/ImagePreview.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+const ImagePreview = ({
+  url,
+  index,
+  onClick
+}: {
+  url: string;
+  index: number;
+  onClick: (index: number) => () => void;
+}) => {
+  return (
+    <StyledContainer>
+      <StyledDeleteButton onClick={onClick(index)}>X</StyledDeleteButton>
+      <StyledImg src={url} />
+    </StyledContainer>
+  );
+};
+
+export default ImagePreview;
+
+const StyledContainer = styled.div`
+  width: fit-content;
+  position: relative;
+`;
+
+const StyledImg = styled.img`
+  width: 100%;
+`;
+
+const StyledDeleteButton = styled.button`
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 25px;
+  height: 25px;
+  color: white;
+  background-color: #ff4d4f;
+  border: none;
+  font-size: 1rem;
+  cursor: pointer;
+`;
