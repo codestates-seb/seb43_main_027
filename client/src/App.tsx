985d9e685fea78b50352058371f310bab58b69d7
crowwan <crowwan@naver.com>
2023-05-16 11:05:35 +0900

    :sparkles: feat: 게시글 작성페이지 라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,7 @@ import CategoryGames from './pages/CategoryGames';
 import Home from './pages/Home';
 import SignUp from './pages/SignUp/SignUp';
 import GameChannel from './pages/GameChannel';
+import Posting from './pages/Posting';
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
           <Route path='/category/:id' element={<CategoryGames />} />
           <Route path='/games/:id' element={<GameChannel />} />
           <Route path='/signup' element={<SignUp />} />
+          <Route path='/post' element={<Posting />} />
         </Route>
       </Routes>
     </>
