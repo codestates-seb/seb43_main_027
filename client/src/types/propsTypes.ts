433230004a58e105ff24266b9413385cca199d7f
한수지 <fatholic5@gmail.com>
2023-05-16 10:10:20 +0900

    :label: type: 게임아이템 및 카테고리태그 프롭 타입 추가

--- client/src/types/propsTypes.ts
+++ client/src/types/propsTypes.ts
@@ -1,4 +1,4 @@
-import SwiperInstance from 'swiper';
+import { type CategoryType } from './dataTypes';
 
 export type NavStateType = {
   setShow: React.Dispatch<React.SetStateAction<boolean>>;
@@ -22,4 +22,16 @@ export type SwiperInfoType = {
 
 export type TabSelectType = {
   isSelectTab: string;
+};
+
+export type GameItemPropsType = {
+  gameId: number, 
+  gameName: string,
+  followerCount: number,
+  categories: CategoryType[],
+  mainImgUrl: string,
+}
+
+export type StyledTagPropsType = {
+  styleId: number;
 };
