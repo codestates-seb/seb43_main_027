63329ab57b3dd14715a773a1e24cfdb3b1bfc517
UseonJ <naul630@gmail.com>
2023-05-17 01:09:56 +0900

    :recycle: refactor: 불필요한 prop 제거, 온클릭 함수 분리

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -1,7 +1,3 @@
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import useInput from '../../hooks/useInput';
-
 import styled from 'styled-components';
 
 import SignUpFieldsContainer from './SignUpFieldsContainer';
@@ -9,44 +5,10 @@ import SignUpTopWrapper from './SignUpTopWrapper';
 import SignUpOauthContainer from './SignUpOauthContainer';
 import SignUpButtonsContainer from './SignUpButtonsContainer';
 
+import oauthSignUp from '../../utils/OauthSignUpFunction';
+import emailSignUp from '../../utils/EmailSignUpFunction';
+
 const SignUp = () => {
-  const navigator = useNavigate();
-
-  const userNameProps = useInput('', 'username');
-  const emailProps = useInput('', 'email');
-  const passWordProps = useInput('', 'password');
-  // 현재 유효성검사 useInput에 포함되어서 처리 중인데,
-  // 관심사 분리에 따라 useValidation으로 분리해서 하는 것도 좋아보임.
-
-  const oauthSignUp: React.MouseEventHandler = async (e: React.MouseEvent) => {
-    e.preventDefault();
-    const googleAuthUrl = `${process.env.REACT_APP_SERVER}/oauth2/authorization/google?redirect_uri=http://localhost:3000/signup`;
-    window.location.href = googleAuthUrl;
-  };
-
-  const emailSignUp: React.MouseEventHandler = async (e: React.MouseEvent) => {
-    e.preventDefault();
-    try {
-      await axios
-        .post(
-          'http://ec2-13-209-70-188.ap-northeast-2.compute.amazonaws.com:8080/api/members/signup',
-          {
-            username: userNameProps.value,
-            email: emailProps.value,
-            password: passWordProps.value
-          }
-        )
-        .then((response) => {
-          alert('you successfully signed up!');
-          navigator('/login');
-        });
-    } catch (error) {
-      console.log(error);
-      alert('you failed to signup!');
-      navigator('/error');
-    }
-  };
-
   return (
     <StyledSignUpContainer>
       <StyledSignUpFormWrapper>
@@ -57,11 +19,7 @@ const SignUp = () => {
           {/* Oauth - component */}
           <SignUpOauthContainer onClick={oauthSignUp} />
           {/* Input - components */}
-          <SignUpFieldsContainer
-            inputUserName={userNameProps}
-            inputEmail={emailProps}
-            inputPassWord={passWordProps}
-          />
+          <SignUpFieldsContainer />
           {/* Button - components */}
           <SignUpButtonsContainer onClick={emailSignUp} />
         </StyledSignUpFormContainer>
