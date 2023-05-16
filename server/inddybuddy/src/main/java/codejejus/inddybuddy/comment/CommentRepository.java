61c4e4c5f0e047e822e05ea5e18d651eb5c27cd7
zqwz <zqwz@naver.com>
2023-05-16 15:25:12 +0900

    style : 코드 포매팅

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/comment/CommentRepository.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/comment/CommentRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     Page<Comment> findAll(Pageable pageable);
+
     Page<Comment> findByParentComment(Long parentComment, Pageable pageable);
 }
