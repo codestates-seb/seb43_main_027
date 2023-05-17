6e735c5784ecc4e0d8977f9ea85d01875574644d
한수지 <fatholic5@gmail.com>
2023-05-18 07:10:25 +0900

    :label: type: 게임페이지에서 응답받는 게시글타입 추가

--- client/src/types/dataTypes.ts
+++ client/src/types/dataTypes.ts
@@ -52,11 +52,55 @@ export type PathUrlType = {
   POSTING: string;
 };
 
-export type PostOptinType = {
+export type PostOptionType = {
   value: string;
   label: string;
 };
 
 export type OptionMapping = {
   [key: string]: string;
+};
+
+/*
+특정게임의 게시글 전체 조회할때 받는 응답데이터의 타입
+member에 username은 작성자 이름이라 필요하고,
+
+유저 북마크게시글,
+내가쓴글 get데이터에 gameId가 있으면 특정게임내 페이지에서 필터링 가능할듯
+
+export type PostType = {
+  postId: number;
+  member: {
+    username: string;
+  };
+  title: string;
+  views: number;
+  postTag: string;
+  commentCount: number;
+  likeCount: number;
+  createdAt?: string;
+  gameId?: number;
+  username?: string;
+};
+*/
+
+export type GamePagePostType = {
+  postId: number;
+  member?: {
+    memberId?: number;
+    email?: string;
+    username: string;
+    imageUrl?: string;
+    followerCount?: number;
+    followingCount?: number;
+  };
+  title: string;
+  content?: string;
+  views: number;
+  postTag: string;
+  commentCount: number;
+  likeCount: number;
+  gameId?: number;
+  createdAt?: string;
+  username?: string;
 };
