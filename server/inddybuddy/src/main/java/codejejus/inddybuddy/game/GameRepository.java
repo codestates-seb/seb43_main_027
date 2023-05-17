b279774758153fcd6141e43d3cbaf811f77a085e
zqwz <zqwz@naver.com>
2023-05-17 17:27:06 +0900

    refactor : Game과 Categories의 EntityGraph 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/game/GameRepository.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/game/GameRepository.java
@@ -2,11 +2,13 @@ package codejejus.inddybuddy.game;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long> {
 
+    @EntityGraph(attributePaths = "categories", type = EntityGraph.EntityGraphType.FETCH)
     Page<Game> findByGameNameContaining(String keyword, Pageable pageable);
 }
