e42598759db8ab2aa1b0ada3e8751bfc02bba218
UseonJ <naul630@gmail.com>
2023-05-18 02:29:11 +0900

    :construction: wip: 게임등록시 사용할 데이터

--- client/src/data/gameTags.ts
+++ client/src/data/gameTags.ts
@@ -1,7 +1,39 @@
-export const gameTags:string[] = [
+const gameTags:string[] = [
 '액션','어드벤쳐','카드/보드게임','코미디','FPS',
 '핵앤슬래쉬','힐링','호러', 'MOBA', '모바일', 
 '멀티플레이', '플랫포머', '퍼즐', '로그라이크', 'RPG', 
 '슈팅', '시뮬레이션', '스포츠', '스토리', '전략',
 '생존', '3D 게임', '2D 게임', '턴제'
 ]
+
+const textTranslate = {
+  FPS: 'FPS',
+  RPG: 'RPG',
+  ADVENTURE: '어드벤쳐',
+  SHOOTING: '슈팅',
+  SIMULATION: '시뮬레이션',
+  ACTION: '액션',
+  SPORTS: '스포츠',
+  STRATEGY: '전략',
+  CARD_AND_BOARD: '카드 및 보드게임',
+  PUZZLE: '퍼즐',
+  STORY: '스토리',
+  HEALING: '힐링',
+  HORROR: '공포',
+  COMEDY: '개그',
+  HACK_AND_SLASH: '핵앤슬',
+  TURN_BASED: '턴제',
+  SURVIVAL: '생존',
+  MULTIPLAYER: '멀티',
+  ROGUELIKE: '로그라이크',
+  PLATFORMER: '플랫포머',
+  TWO_D_GAME: '2D 게임',
+  THREE_D_GAME: '3D 게임',
+  MOBA: 'MOBA',
+  MOBILE: '모바일',
+  OTHER: '기타'
+};
+
+export const gameTagInfo = {
+  gameTags, textTranslate
+}
