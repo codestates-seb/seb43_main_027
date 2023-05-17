9b67c862437b2c1c23fcaf28d7b32912f5dd5e94
crowwan <crowwan@naver.com>
2023-05-17 15:43:04 +0900

    :sparkles: feat: 태그 한글 및 영문 변경 함수 추가

---
+++ client/src/utils/convertTag.ts
@@ -0,0 +1,30 @@
+type TagMap = {
+  [key: string]: string;
+};
+
+const tagMap: TagMap = {
+  RECRUITMENT: '모집',
+  BUG: '버그',
+  WALKTHROUGH: '공략',
+  CHIT_CHAT: '수다',
+  INFORMATION: '정보',
+  FAN_ART: '팬아트',
+  QUESTION: '질문',
+  SHOWING_OFF: '자랑하기',
+  REVIEW: '리뷰',
+  ETC: '기타'
+};
+
+const convertTag = {
+  asKR(tag: string) {
+    return tagMap[tag];
+  },
+  asEN(tag: string) {
+    for (const [k, v] of Object.entries(tagMap)) {
+      if (v === tag) return k;
+    }
+    return false;
+  }
+};
+
+export default convertTag;
