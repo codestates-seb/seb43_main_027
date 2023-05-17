f26a6904d9b27a12f69fddf747cec41570e515a7
UseonJ <naul630@gmail.com>
2023-05-17 15:51:48 +0900

    :construction: wip: 함수 외부로 빼는작업 진행중

--- client/src/utils/EmailSignUpFunction.ts
+++ client/src/utils/EmailSignUpFunction.ts
@@ -1,27 +1,29 @@
-import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-
-
-const navigator = useNavigate();
+import { useSelector } from 'react-redux';
+import { RootState } from '../store/store';
 
 const emailSignUp: React.MouseEventHandler = async (e: React.MouseEvent) => {
+  const signupinfo = useSelector((state:RootState) => state.signup);
+  console.log(signupinfo)
   e.preventDefault();
   try {
     await axios
       .post(
         'http://ec2-13-209-70-188.ap-northeast-2.compute.amazonaws.com:8080/api/members/signup',
         {
-          
+          username: signupinfo.username, 
+          email: signupinfo.email, 
+          password: signupinfo.password 
         }
       )
       .then((response) => {
         alert('you successfully signed up!');
-        navigator('/login');
+        // Navigate('/login');
       });
   } catch (error) {
     console.log(error);
     alert('you failed to signup!');
-    navigator('/error');
+    // navigator('/error');
   }
 };
 
--- client/src/utils/OauthSignUpFunction.ts
+++ client/src/utils/OauthSignUpFunction.ts
@@ -1,6 +1,6 @@
 const oauthSignUp: React.MouseEventHandler = async (e: React.MouseEvent) => {
   e.preventDefault();
-  const googleAuthUrl = `${process.env.REACT_APP_SERVER}/oauth2/authorization/google?redirect_uri=http://localhost:3000/signup`;
+  const googleAuthUrl = `${process.env.REACT_APP_API_URL}/oauth2/authorization/google?redirect_uri=http://localhost:3000/signup`;
   window.location.href = googleAuthUrl;
 };
 
