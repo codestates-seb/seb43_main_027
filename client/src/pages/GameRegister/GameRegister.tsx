4e1277263c33766fd9d8f40e1e63f7516eaadc86
UseonJ <naul630@gmail.com>
2023-05-18 01:35:16 +0900

    :fire: remove: antd 제거

--- client/src/pages/GameRegister/GameRegister.tsx
+++ client/src/pages/GameRegister/GameRegister.tsx
@@ -1,5 +1,4 @@
 import { UploadOutlined } from '@ant-design/icons';
-import { Button, Input, Form, Space, Upload } from 'antd';
 import Label from '../../components/elements/Label';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
@@ -8,14 +7,15 @@ import axios from 'axios';
 import styled from 'styled-components';
 
 import GameTagsContainer from '../../components/GameRegister/GameTagsContainer';
-
-const { TextArea } = Input;
+import { gameTags } from '../../data/gameTags';
 
 const GameRegister = () => {
   const navigation = useNavigate();
   const [title, setTitle] = useState('');
   const [detail, setDetail] = useState('');
   const [url, setUrl] = useState('');
+  const [tagStates, setTagStates] = useState<boolean[]>([]);
+  const [files, setFiles] = useState<File[]>([]);
 
   const titleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTitle(e.target.value);
@@ -28,17 +28,13 @@ const GameRegister = () => {
     setUrl(e.target.value);
   };
 
-  const formItemLayout = {
-    labelCol: { span: 6 },
-    wrapperCol: { span: 14 }
-  };
+  const handlefile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0] || null;
 
-  const normFile = (e: any) => {
-    console.log('Upload event:', e);
-    if (Array.isArray(e)) {
-      return e;
+    console.log(file);
+    if (file) {
+      setFiles([...files, file]);
     }
-    return e?.fileList;
   };
 
   interface PostDataType {
@@ -47,32 +43,39 @@ const GameRegister = () => {
     categoryNames: string[];
   }
 
-  const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
-
-    const token = localStorage.getItem('access_token');
-
+  const submitFormData = (e: React.FormEvent) => {
+    e.preventDefault();
+    const reducer = (a: number[], c: boolean, i: number) => {
+      if (c === true) a.push(i);
+      return a;
+    };
+    const selectedTagsIndex = tagStates.reduce(reducer, []);
+    const selectedTags = selectedTagsIndex.map((i) => gameTags[i]);
     const postData: PostDataType = {
       gameName: title,
       downloadUrl: url,
-      categoryNames: ['FPS']
+      categoryNames: selectedTags
     };
     console.log(postData);
 
     const formData = new FormData();
-    // console.log(values.gametitleimg[0]);
-    // formData.append('file', values.gametitleimg[0]);
-
-    // const blob = new Blob([JSON.stringify(postData)], {
-    //   type: 'application/json'
-    // });
-    // console.log(blob);
     formData.append('post', JSON.stringify(postData));
-    console.log(typeof formData);
-    const value = formData.values().next();
+
+    for (const afile of files) {
+      formData.append('file', afile);
+    }
+
+    let value = formData.values().next();
+    console.log(value);
+    value = formData.values().next();
+    console.log(value);
+    value = formData.values().next();
+    console.log(value);
+    value = formData.values().next();
     console.log(value);
 
-    // headers 를 변수로 담아줍니다.
+    // headers를 변수로 담아줍니다.
+    const token = localStorage.getItem('access_token');
     const headers = {
       'Content-Type': 'multipart/form-data',
       Authorization: `${token}` // 토큰을 헤더에 추가
@@ -80,40 +83,24 @@ const GameRegister = () => {
 
     // formData를 axios를 사용하여 POST합니다.
     axios
-      .post(
-        'http://ec2-13-209-70-188.ap-northeast-2.compute.amazonaws.com:8080/api/games',
-        formData,
-        { headers }
-      )
+      .post(`${process.env.REACT_APP_API_URL}/api/games`, formData, { headers })
       .then((response) => {
-        // 성공적으로 요청을 처리한 후에 수행할 작업을 여기에 추가합니다.
         console.log('등록에 성공하였습니다');
         navigation(-1);
-        // message.success('요청이 성공적으로 전송되었습니다.');
       })
       .catch((error) => {
-        // 요청이 실패한 경우에 수행할 작업을 여기에 추가합니다.
         console.error(error);
-        // message.error('요청을 전송하는 중에 오류가 발생했습니다.');
+        navigation('/error');
       });
   };
 
   return (
     <StyledFormContainer>
-      <Form
-        name='validate_other'
-        {...formItemLayout}
-        onFinish={onFinish}
-        initialValues={{
-          'input-number': 3,
-          'checkbox-group': ['A', 'B'],
-          rate: 3.5
-        }}
-      >
+      <StyledForm onSubmit={submitFormData}>
         {/* 게임 제목 */}
         <StyledGameNameContainer>
           <Label htmlFor='channeltitle'>채널(게임) 이름</Label>
-          <Input
+          <input
             name='channeltitle'
             placeholder='ex)Crypt of Necrodancer'
             className='colorchange'
@@ -122,11 +109,11 @@ const GameRegister = () => {
           />
         </StyledGameNameContainer>
         {/* 태그 선택 */}
-        <GameTagsContainer />
+        <GameTagsContainer tagStates={tagStates} setTagStates={setTagStates} />
         {/* 게임 설명 */}
         <StyledGameDetailContainer>
           <Label htmlFor='channeltitle'>게임 설명</Label>
-          <TextArea
+          <textarea
             rows={6}
             placeholder='ex)Crypt of Necrodancer는 주인공이 리듬에 맞춰 던젼을 탐험하며 아버지의 유산과 자신의 존재의미를 찾는 여정을 담은 rpg와 로그라이크 요소가 결합된 리듬게임입니다.'
             maxLength={600}
@@ -138,7 +125,7 @@ const GameRegister = () => {
         {/* 다운로드 URL */}
         <StyledGameNameContainer>
           <Label htmlFor='downloadurl'>다운로드 링크</Label>
-          <Input
+          <input
             name='downloadurl'
             placeholder='ex)게임 홈페이지 주소'
             className='colorchange'
@@ -150,39 +137,35 @@ const GameRegister = () => {
         {/* 게임 대표이미지 */}
         <StyledImageContainer>
           <Label htmlFor='gametitleimg'>게임대표사진</Label>
-          <Form.Item
-            name='gametitleimg'
-            valuePropName='fileList'
-            getValueFromEvent={normFile}
-            extra='png,jpg 등'
-          >
-            <Upload name='logo' action='/upload.do' listType='picture'>
-              <Button icon={<UploadOutlined />}>파일 업로드</Button>
-            </Upload>
-          </Form.Item>
+          <input name='gametitleimg' type='file' onChange={handlefile} />
+          <button>파일 업로드</button>
         </StyledImageContainer>
         {/* 확인/취소 버튼 */}
         <StyledButtonsContainer>
-          <Form.Item>
-            <Space>
-              <Button type='primary' htmlType='submit' className='enroll'>
-                등록
-              </Button>
-              <Button htmlType='reset' className='cancel'>
-                취소
-              </Button>
-            </Space>
-          </Form.Item>
+          <button type='submit' className='enroll'>
+            등록
+          </button>
+          <button type='button' className='cancel'>
+            취소
+          </button>
         </StyledButtonsContainer>
-      </Form>
+      </StyledForm>
     </StyledFormContainer>
   );
 };
 
 export default GameRegister;
 
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 const StyledFormContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100%;
