c6a989c5e9523c4fe81ac2fcbd9e7e0895254cdf
UseonJ <naul630@gmail.com>
2023-05-18 01:39:32 +0900

    :tada: init: 머지

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,5 @@
 import GlobalStyle from './styles/globalStyles';
-
-import { Route, Routes } from 'react-router-dom';
-import Template from './pages/Template';
-import CategoryGames from './pages/CategoryGames';
-import Home from './pages/Home';
-import SignUp from './pages/SignUp/SignUp';
-import LogIn from './pages/LogIn/LogIn';
-import GameChannel from './pages/GameChannel';
+import AppRoutes from './AppRoutes';
 
 const App = () => {
   return (
