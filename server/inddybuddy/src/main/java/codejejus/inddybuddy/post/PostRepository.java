3ed6f73214a2b7dc966e32d6ee5bbfe62b73a2f2
zqwz <zqwz@naver.com>
2023-05-17 17:26:38 +0900

    refactor : Post와 Member의 EntityGraph 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/post/PostRepository.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/post/PostRepository.java
@@ -2,13 +2,18 @@ package codejejus.inddybuddy.post;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     Page<Post> findAll(Pageable pageable);
 
+    @EntityGraph(attributePaths = {"member", "member.file"}, type = EntityGraph.EntityGraphType.FETCH)
     Page<Post> findAllByPostTag(Post.PostTag postTag, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"member", "member.file"}, type = EntityGraph.EntityGraphType.FETCH)
     Page<Post> findAllByContentContainingOrTitleContaining(String content, String title, Pageable pageable);
 }
+
