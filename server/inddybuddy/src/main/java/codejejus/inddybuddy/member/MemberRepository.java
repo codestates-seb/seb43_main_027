b128914004a4ff0e788f1f19235f5f213b0ddbe1
zqwz <zqwz@naver.com>
2023-05-17 17:21:49 +0900

    feat : 회원 이름으로 검색 기능 구현

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/member/MemberRepository.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/member/MemberRepository.java
@@ -1,6 +1,8 @@
 package codejejus.inddybuddy.member;
 
 import codejejus.inddybuddy.member.entity.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -12,4 +14,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByUsername(String username);
 
     boolean existsByEmail(String email);
+
+    Page<Member> findByUsernameContaining(String keyword, Pageable pageable);
 }
