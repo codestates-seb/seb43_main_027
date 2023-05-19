f68b87699e2eb93feb8895d410fe4ee4b6bdd5c7
UseonJ <naul630@gmail.com>
2023-05-19 13:30:36 +0900

    :recycle: refactor: 커스텀훅 수정

--- client/src/hooks/useInput.ts
+++ client/src/hooks/useInput.ts
@@ -1,38 +1,32 @@
 import { useState } from 'react';
-import { useDispatch } from 'react-redux';
-
-import validation from './validation';
-import { validationType } from '../types/utilTypes';
-
-import { setSignupInfo } from '../slice/signupSlice';
+import useValidation from './useValidation';
+import { ValidationFunction } from '../types/customHooksTypes';
 
 /**
  * input 상태를 관리하는 커스텀 훅입니다.
  * @param {string} init - input 창의 초기값 (default: '')
- * @param {string} iputtype - input 창의 유형
- * @returns {Array} - input 상태, input 값 변경 시 핸들러, input 상태 변경함수, input 창의 유효성
+ * @param {function extraActionWithValue((params:string) {}:void)} - e.target.value를 store에 저장하는 등 e.target.value로 필요한 추가적인 액션을 하는 void 함수
+ * @param {function customValidationFunction((params:string) {}:boolean)} - e.target.value의 유효성 검사를 진행할 함수
+ * @returns {Object} - input value(상태), input에서 작동할 onChangehandler, input 창의 유효성검사 결과
  */
-let data;
 
-function useInput(init = '',validationType:validationType,customValidationFunction?:(value:string) => boolean) {
+function useInput(init = '', extraActionWithValue?:(value:string)=>void ,
+                  customValidationFunction?:ValidationFunction) {
   const [value, setValue] = useState(init);
   const [validity, setValidity] = useState(false);
 
-  const dispatch = useDispatch();
-
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    e.stopPropagation();
+    // e.stopPropagation();
     setValue(e.target.value);
-    setValidity(validation(validationType,e.target.value,customValidationFunction));
-
-    //  Signup 관련 store 업데이트
-    if(validationType === 'username') dispatch(setSignupInfo({key:'username', value:e.target.value}))
-    if(validationType === 'email') dispatch(setSignupInfo({key:'email', value:e.target.value}))
-    if(validationType === 'password') dispatch(setSignupInfo({key:'password', value:e.target.value}))
+    if (customValidationFunction) {
+      const validationResult = useValidation(e.target.value,customValidationFunction)
+      if(validationResult) setValidity(validationResult);
+    }
+    if (extraActionWithValue) extraActionWithValue(e.target.value);
     console.log(validity);
   };
-  data = { value, onChange , setValue, validity}
-  return data;
+
+  return { value, onChange, validity}
 }
 
 export default useInput;
