e5bb6b05a05fa2f9ebf68c14ee12d83c21c7d907
UseonJ <naul630@gmail.com>
2023-05-21 00:16:36 +0900

    :construction: wip: useEffect와 undefined의 듀엣
    얼른 해결하겠습니다

--- client/src/components/Login/googleLogin.tsx
+++ client/src/components/Login/googleLogin.tsx
@@ -1,19 +1,107 @@
+import { useEffect, useState } from 'react';
 import Loading from '../common/Loading';
 import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../slice/userSlice';
 
 const GoogleLogIn = () => {
   const navigator = useNavigate();
 
-  const url = new URL(window.location.href);
-  const accessToken = url.searchParams.get('token');
-  if (accessToken) {
-    localStorage.setItem('access_token', accessToken);
-    navigator('/');
-  } else {
-    alert('로그인에 실패하였습니다.');
-    console.log('Access token이 없습니다.');
-    navigator('/signup');
-  }
+  const dispatch = useDispatch();
+
+  const [accessToken, setAccessToken] = useState('');
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    if (!localStorage.getItem('accessToken')) {
+      const param = url.searchParams.get('token');
+      if (param) {
+        console.log(`this is param: ${param}`);
+        setAccessToken(param);
+      }
+    } else {
+      console.log('there is access token in localStorage');
+      setAccessToken(localStorage.getItem('accessToken')!);
+    }
+  }, []);
+  // const url = new URL(window.location.href);
+  // let accessToken = '';
+  // if (!accessToken && !localStorage.getItem('accessToken')) {
+  //   const param = url.searchParams.get('token');
+  //   if (param) accessToken = param;
+  // }
+  // const token = localStorage.getItem('accessToken');
+  // if (token) {
+  //   accessToken = token;
+  // }
+  // if (accessToken) {
+  //   localStorage.setItem('access_token', accessToken);
+  //   console.log('홈으로 이동합니다');
+  //   navigator('/');
+  // } else {
+  //   alert('로그인에 실패하였습니다.');
+  //   console.log('Access token이 없습니다.');
+  //   navigator('/signup');
+  // }
+
+  // useEffect(() => {
+  //   localStorage.setItem('access_token', `Bearer ${accessToken}`);
+  //   console.log(accessToken);
+  //   const headers = {
+  //     Authorization: `Bearer ${accessToken}`
+  //   };
+  //   try {
+  //     axios(`${process.env.REACT_APP_API_URL}/api/members/profile`, {
+  //       headers
+  //     }).then((response) => {
+  //       localStorage.setItem('access_token', response.headers.authorization);
+  //       localStorage.setItem('refresh_token', response.headers.refresh);
+  //       const userdata = response.data;
+  //       dispatch(setUser({ ...userdata }));
+  //       localStorage.setItem('user', JSON.stringify(userdata));
+  //       console.log(JSON.parse(localStorage.getItem('user')!));
+  //       alert('you successfully logged in!');
+  //       navigator('/');
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //     alert('you failed to login!');
+  //     navigator('/error');
+  //   }
+  // }, [accessToken]);
+  useEffect(() => {
+    if (!accessToken) {
+      const actoken = localStorage.getItem('access_token');
+      if (actoken) setAccessToken(actoken);
+    }
+    if (accessToken) {
+      localStorage.setItem('access_token', `Bearer ${accessToken}`);
+      console.log(accessToken);
+      const headers = {
+        Authorization: `Bearer ${accessToken}`
+      };
+      try {
+        axios(`${process.env.REACT_APP_API_URL}/api/members/profile`, {
+          headers
+        }).then((response) => {
+          localStorage.setItem('access_token', response.headers.authorization);
+          localStorage.setItem('refresh_token', response.headers.refresh);
+          const userdata = response.data;
+          dispatch(setUser({ ...userdata }));
+          localStorage.setItem('user', JSON.stringify(userdata));
+          console.log(JSON.parse(localStorage.getItem('user')!));
+          alert('you successfully logged in!');
+          navigator('/');
+        });
+      } catch (error) {
+        console.log(error);
+        alert('you failed to login!');
+        navigator('/error');
+      }
+    }
+  }, [accessToken]);
 
   return (
     <>
