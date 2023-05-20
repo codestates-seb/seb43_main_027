00f71d746c581d5068bf8c49d07cf6e101329b92
crowwan <crowwan@naver.com>
2023-05-20 09:54:09 +0900

    :sparkles: feat: 게임 채널 페이지네이션 구현

--- client/src/components/Home/GameContainer.tsx
+++ client/src/components/Home/GameContainer.tsx
@@ -1,17 +1,82 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import FilterBar from './FilterBar';
 import HomeGameCard from './HomeGameCard';
 
-import { GameType } from '../../types/dataTypes';
+import { GameType, PageInfoType } from '../../types/dataTypes';
+import Pagination from 'react-js-pagination';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import axios from 'axios';
 
 const GameContainer = () => {
+  const user = useSelector((s: RootState) => s.user);
   const [games, setGames] = useState<GameType[]>([]);
+  const [curPage, setCurPage] = useState(1);
+  const [pageInfo, setPageInfo] = useState<PageInfoType>({
+    page: 1,
+    size: 10,
+    totalPage: 1,
+    totalSize: 1
+  });
+
+  const [tabInd, setTabInd] = useState<number>(0);
+
+  const apiRef = useRef([
+    '/api/games/?filter=POPULAR',
+    '/api/games/?filter=NEW',
+    `/api/members/${user.memberId}/mygame`
+  ]);
+
+  const onClickHandler = (i: number) => () => {
+    setTabInd(i);
+  };
+  const onPageClickHandler = (page: number) => {
+    setCurPage(page);
+  };
+
+  useEffect(() => {
+    if (tabInd === 0 || tabInd === 1) {
+      (async () => {
+        try {
+          const res = await axios.get(
+            `${process.env.REACT_APP_API_URL}${apiRef.current[tabInd]}?page=${curPage}&size=${pageInfo.size}`
+          );
+          setGames(res.data.data);
+          setPageInfo(res.data.pageInfo);
+        } catch (err) {
+          console.error(err);
+        }
+      })();
+    } else {
+      (async () => {
+        try {
+          const res = await axios.get(
+            `${process.env.REACT_APP_API_URL}${apiRef.current[tabInd]}`
+          );
+          const newPageInfo = {
+            ...pageInfo,
+            page: curPage,
+            totalSize: res.data.data.length,
+            totalPage: Math.ceil(res.data.data.length / 10)
+          };
+
+          const startInd = (newPageInfo.page - 1) * newPageInfo.size;
+          const endInd = startInd + newPageInfo.size;
+          setGames(res.data.data.slice(startInd, endInd));
+          setPageInfo(res.data.pageInfo);
+        } catch (err) {
+          console.error(err);
+        }
+      })();
+    }
+  }, [tabInd, curPage]);
+
   return (
     <StyledWrapper>
       <StyledContainer>
-        <FilterBar setGames={setGames} />
+        <FilterBar onClickHandler={onClickHandler} tabInd={tabInd} />
         <StyledCardContainer>
           {games.length === 0 && (
             <StyledEmptyItem>등록된 게임 채널이 없습니다.</StyledEmptyItem>
@@ -20,6 +85,19 @@ const GameContainer = () => {
             <HomeGameCard key={game.gameId} {...game} />
           ))}
         </StyledCardContainer>
+        {games.length > 0 && (
+          <StyledPagination>
+            <Pagination
+              activePage={curPage}
+              itemsCountPerPage={pageInfo.size}
+              totalItemsCount={pageInfo.totalSize}
+              pageRangeDisplayed={5}
+              prevPageText={'‹'}
+              nextPageText={'›'}
+              onChange={onPageClickHandler}
+            />
+          </StyledPagination>
+        )}
       </StyledContainer>
     </StyledWrapper>
   );
@@ -63,4 +141,7 @@ const StyledEmptyItem = styled.div`
   font-size: 18px;
   font-weight: 700;
   color: var(--default-text-color);
+`;
+const StyledPagination = styled.div`
+  padding: 20px;
 `;
