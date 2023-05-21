0a64b88359f0789d898d50d89e6425aaebd81bf3
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-05-22 03:16:12 +0900

    :sparkles: feat: 회원삭제 기능 추가 회원삭제 버튼 클릭시 모달로 확인 받은후, 회원 데이터 삭제 요청 후 홈으로 새로고침 이동됨

--- client/src/components/UserInfo/UserEditInfo.tsx
+++ client/src/components/UserInfo/UserEditInfo.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import UserProfileImg from './UserProfileImg';
 import UserProfileName from './UserProfileName';
@@ -9,10 +9,12 @@ import { StyledTitleWrapper, StyledAboutMe } from './UserTitle';
 import CreateChannelButton from '../ui/CreateChannelButton';
 import { CiCircleRemove } from 'react-icons/ci';
 import { UserInfoProps } from '../../types/propsTypes';
+import PATH_URL from '../../constants/pathUrl';
 
 const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
   
   const { memberId } = useParams();
+  const navigate = useNavigate();
   const [ isUserImg, setIsUserImg ] = useState<string>('');
   const [ isUserName, setIsUserName ] = useState<string>('');
   const [ isUserEmail, setIsUserEmail ] = useState<string>('');
@@ -56,6 +58,36 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
     window.location.reload();
   };
 
+  const handleRemoveClick = () => {
+    const result = window.confirm('회원탈퇴를 선택하시면 모든 계정 정보가 영구적으로 삭제됩니다.\n계속 진행하시겠습니까?');
+  
+    if (result) {
+      const token = localStorage.getItem('access_token');
+      axios
+      .delete(
+        `${process.env.REACT_APP_API_URL}/api/members/${memberId}`,
+        {
+          headers: {
+            Authorization: `${token}`
+          }
+        }
+      )
+      .then((response) => {
+        alert('회원탈퇴가 완료되었습니다. 이용해주셔서 감사합니다.');
+        localStorage.removeItem('user');
+        localStorage.removeItem('access_token');
+        localStorage.removeItem('refresh_token');
+        navigate(`${PATH_URL.HOME}`);
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.error('언팔로우 요청 실패:', error);
+      });
+    } else {
+      return;
+    };
+  };
+
   return (
     <StyledTitleWrapper>
       <UserProfileImg isUserImg={isUserImg} />
@@ -69,7 +101,9 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
       <StyledEditAboutMe>
         <UserAboutMe />
       </StyledEditAboutMe>
-      <p>회원탈퇴</p>
+      <StyledRemoveBtn onClick={handleRemoveClick} >
+        회원탈퇴
+      </StyledRemoveBtn>
     </StyledTitleWrapper>
   );
 };
@@ -96,4 +130,12 @@ const StyledCancleContain = styled.div`
   &:hover {
     color: var(--button-inactive-hover-color);
   }
+`;
+
+const StyledRemoveBtn = styled.div`
+  margin-top: 20px;
+  font-size: 14px;
+  color: var(--default-text-color);
+  cursor: pointer;
+  border-style: none;
 `;
