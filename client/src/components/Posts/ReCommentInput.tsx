b77c5ec5035850e0b202773d654f3167a45bdeb0
crowwan <crowwan@naver.com>
2023-05-21 20:27:42 +0900

    :sparkles: feat: 대댓글 작성 기능 구현 완료

--- client/src/components/Posts/ReCommentInput.tsx
+++ client/src/components/Posts/ReCommentInput.tsx
@@ -2,18 +2,53 @@ import { useRef } from 'react';
 import { useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { RootState } from '../../store/store';
+import { useParams } from 'react-router-dom';
+import { postData } from '../../api/apiCollection';
 
-const ReCommentInput = () => {
-  const commentRef = useRef('');
+const ReCommentInput = ({
+  parentCommentId,
+  onCommentSubmit
+}: {
+  parentCommentId: number;
+  onCommentSubmit: () => void;
+}) => {
+  const commentRef = useRef<HTMLInputElement>(null);
+  const { gameId, postId } = useParams();
   const user = useSelector((s: RootState) => s.user);
 
+  const onClickHandler = () => {
+    if (commentRef?.current?.value.length === 0) return;
+    postData(
+      `${process.env.REACT_APP_API_URL}/api/games/${gameId}/posts/${postId}/comments`,
+      JSON.stringify({
+        content: commentRef?.current?.value
+      }),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('access_token')
+        }
+      },
+      (res) => {
+        if (commentRef && commentRef.current) commentRef.current.value = '';
+        onCommentSubmit();
+      },
+      () => {
+        console.log('test');
+      }
+    );
+  };
   return (
     <StyledContainer>
       <StyledWrapper>
         <StyledImg src={user.imageUrl} />
         <StyledInputBox>
-          <StyledInput type='text' placeholder='댓글을 입력해주세요.' />
-          <StyledButton>등록하기</StyledButton>
+          <StyledInput
+            type='text'
+            placeholder='댓글을 입력해주세요.'
+            ref={commentRef}
+          />
+          <StyledButton onClick={onClickHandler}>등록하기</StyledButton>
         </StyledInputBox>
       </StyledWrapper>
     </StyledContainer>
