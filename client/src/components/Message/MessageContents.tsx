ad2b70b49bf771581df47bdd9c43f99bb3c039a3
crowwan <crowwan@naver.com>
2023-05-22 23:46:23 +0900

    :sparkles: feat: 무한스크롤 기능 구현으로 인한 pageInfo 프롭스 추가

--- client/src/components/Message/MessageContents.tsx
+++ client/src/components/Message/MessageContents.tsx
@@ -3,25 +3,29 @@ import styled from 'styled-components';
 import MessageOutput from './MessageOutput';
 import MessageSend from './MessageSend';
 import { Single } from './SingleMessage';
+import { PageInfoType } from '../../types/dataTypes';
 
 interface MessageContents {
   receiverId: number;
   messageResponse: Single[];
   addPrevMessages: (newData: Single[]) => void;
   addNewMessages: (newData: Single) => void;
+  pageInfo: PageInfoType | null;
 }
 
 const MessageContents = ({
   messageResponse,
   receiverId,
   addPrevMessages,
-  addNewMessages
+  addNewMessages,
+  pageInfo
 }: MessageContents) => {
   return (
     <StyledBodyWrapper>
       <MessageOutput
         messageResponse={messageResponse}
         addPrevMessages={addPrevMessages}
+        pageInfo={pageInfo}
       />
       <MessageSend receiverId={receiverId} addNewMessages={addNewMessages} />
     </StyledBodyWrapper>
