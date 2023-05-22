a3514f3203b542210db7a5c2877bebffb09a5111
crowwan <crowwan@naver.com>
2023-05-22 18:09:49 +0900

    :sparkles: feat: 채팅 슬라이스 생성

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -3,13 +3,15 @@ import userSlice from '../slice/userSlice';
 import categorySlice from '../slice/categorySlice';
 import signupSlice from '../slice/signupSlice';
 import navSlice from '../slice/navSlice';
+import chatSlice from '../slice/chatSlice';
 
 const store = configureStore({
   reducer: {
     user: userSlice,
     category: categorySlice,
     signup: signupSlice,
-    nav: navSlice
+    nav: navSlice,
+    chat: chatSlice
   }
 });
 export type RootState = ReturnType<typeof store.getState>;
