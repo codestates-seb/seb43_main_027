c5a661930113ad7ad6ff10ad8f0d26b8ec7ee50e
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-05-23 05:26:24 +0900

    :sparkles: feat: 회원 리스트 컴포넌트 초기셋팅 추가

---
+++ client/src/components/UserInfo/UserInfoList.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const UserInfoList = () => {
+  return (
+    <StyledWrapper>
+      게시글 정보
+    </StyledWrapper>
+  );
+};
+
+export default UserInfoList;
+
+const StyledWrapper = styled.div`
+
+`;
