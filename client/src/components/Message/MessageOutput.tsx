f0b0463513d8c373483a68fb6a3e3bd4a7c85b90
crowwan <crowwan@naver.com>
2023-05-22 23:45:54 +0900

    :sparkles: feat: 채팅창 처음 렌더링 시 스크롤 하단으로 이동하도록 구현, 이전 채팅 데이터 패칭 기능 구현

--- client/src/components/Message/MessageOutput.tsx
+++ client/src/components/Message/MessageOutput.tsx
@@ -1,42 +1,75 @@
 import styled from 'styled-components';
 
 import SingleMessage, { Single } from './SingleMessage';
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { PageInfoType } from '../../types/dataTypes';
+import { getData } from '../../api/apiCollection';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
 
 interface OutputType {
   messageResponse: Single[];
   addPrevMessages: (newData: Single[]) => void;
+  pageInfo: PageInfoType | null;
 }
 
-const MessageOutput = ({ messageResponse, addPrevMessages }: OutputType) => {
-  const ulRef = useRef<HTMLUListElement>(null);
+const MessageOutput = ({
+  messageResponse,
+  addPrevMessages,
+  pageInfo
+}: OutputType) => {
+  const [lastLi, setLastLi] = useState<Element | null>(null);
+  const [isScroll, setIsScroll] = useState(true);
+  const chatInfo = useSelector((s: RootState) => s.chat);
+  const page = useRef(2);
+
+  const fetchNewMessages = () => {
+    if (pageInfo && pageInfo.totalSize <= page.current) return;
+    getData(
+      `${process.env.REACT_APP_API_URL}/api/messages/${chatInfo.receiver.memberId}?page=${page.current}&size=10`,
+      (res) => {
+        setIsScroll(false);
+        addPrevMessages(res.data.data);
+        page.current += 1;
+      },
+      (err) => {
+        console.error(err);
+      },
+      {
+        headers: {
+          Authorization: localStorage.getItem('access_token')
+        }
+      }
+    );
+  };
 
   useEffect(() => {
-    if (ulRef.current) {
-      ulRef.current.scrollTop = ulRef.current.scrollHeight;
-    }
-  }, [ulRef.current]);
+    if (lastLi && isScroll) lastLi.scrollIntoView({ behavior: 'smooth' });
+    setIsScroll(true);
+  }, [lastLi]);
 
   return (
     <>
-      <button
-        onClick={() => {
-          addPrevMessages([
-            { content: 'test', createdAt: Date().toString(), senderId: 1 }
-          ]);
-        }}
-      >
-        test
-      </button>
-      <StyledUL ref={ulRef}>
-        {messageResponse.map((item, i) => (
-          <SingleMessage
-            key={i}
-            senderId={item.senderId}
-            content={item.content}
-            createdAt={item.createdAt}
-          />
-        ))}
+      <button onClick={fetchNewMessages}>test</button>
+      <StyledUL>
+        {messageResponse.map((item, i) =>
+          i === messageResponse.length - 1 ? (
+            <div key={i} className='last' ref={setLastLi}>
+              <SingleMessage
+                senderId={item.senderId}
+                content={item.content}
+                createdAt={item.createdAt}
+              />
+            </div>
+          ) : (
+            <SingleMessage
+              key={i}
+              senderId={item.senderId}
+              content={item.content}
+              createdAt={item.createdAt}
+            />
+          )
+        )}
       </StyledUL>
     </>
   );
@@ -49,9 +82,14 @@ const StyledUL = styled.ul`
   flex: 1 0 0;
   gap: 4rem;
   flex-direction: column;
-  justify-content: flex-end;
   height: 50rem;
   overflow-y: scroll;
   padding: 0.5rem;
   margin-bottom: 2rem;
+  > :first-child {
+    margin-top: auto;
+  }
+  ::-webkit-scrollbar {
+    display: none;
+  }
 `;
