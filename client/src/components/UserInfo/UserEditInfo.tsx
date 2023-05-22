2a8cf69581c681811fd8f2b1581b13188478a0fe
crowwan <crowwan@naver.com>
2023-05-22 09:15:06 +0900

    :bug: fix: 유저 상태 변경 시 로컬 스토리지에도 바로 반영되도록 수정

--- client/src/components/UserInfo/UserEditInfo.tsx
+++ client/src/components/UserInfo/UserEditInfo.tsx
@@ -11,33 +11,40 @@ import { UserInfoProps } from '../../types/propsTypes';
 import PATH_URL from '../../constants/pathUrl';
 import { UserOutlined } from '@ant-design/icons';
 import { Input } from 'antd';
+import { useDispatch } from 'react-redux';
+import { setUser } from '../../slice/userSlice';
 
 const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
   const { memberId } = useParams();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  const [ isUserImg, setIsUserImg ] = useState<string>('');
-  const [ isUserName, setIsUserName ] = useState<string>('');
-  const [ isEditAboutMe, setIsEditAboutMe ] = useState<string>('');
+  const [isUserImg, setIsUserImg] = useState<string>('');
+  const [isUserName, setIsUserName] = useState<string>('');
+  const [isEditAboutMe, setIsEditAboutMe] = useState<string>('');
 
   const fileInputRef = useRef<HTMLInputElement>(null);
-  const [ selectedFile, setSelectedFile ] = useState<File | null>(null);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   const { TextArea } = Input;
 
   useEffect(() => {
     const fetchUserData = async () => {
       const getMemberData = localStorage.getItem('user');
-      const memberData = getMemberData ? JSON.parse(getMemberData) : { memberId: -1 };
+      const memberData = getMemberData
+        ? JSON.parse(getMemberData)
+        : { memberId: -1 };
       const loginId = memberData.memberId;
-  
+
       if (memberId !== loginId.toString()) {
         setIsEditClick(false);
         return;
-      };
+      }
 
       try {
-        const res = await axios.get(`${process.env.REACT_APP_API_URL}/api/members/${memberId}/profile`);
+        const res = await axios.get(
+          `${process.env.REACT_APP_API_URL}/api/members/${memberId}/profile`
+        );
         const fetchedData = res.data.data;
 
         console.log(fetchedData);
@@ -45,18 +52,17 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
         setIsUserImg(fetchedData.imageUrl);
         setIsUserName(fetchedData.userName);
         setIsEditAboutMe(fetchedData.aboutMe);
-
       } catch (error) {
         console.log(error);
-      };
+      }
     };
 
     fetchUserData();
-  } , [memberId]);
+  }, [memberId]);
 
   if (isUserName === null) {
     setIsUserName('등록된 닉네임이 없습니다.');
-  };
+  }
 
   // 입력폼 핸들러
   const handleUploadImg = (event: ChangeEvent<HTMLInputElement>) => {
@@ -80,28 +86,36 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
   const handleSubmitEditClick = () => {
     const formData = new FormData();
 
-    const jsonBlob = new Blob([JSON.stringify({
-      username: isUserName,
-      aboutMe: isEditAboutMe
-    })], { type: 'application/json' });
-    
+    const jsonBlob = new Blob(
+      [
+        JSON.stringify({
+          username: isUserName,
+          aboutMe: isEditAboutMe
+        })
+      ],
+      { type: 'application/json' }
+    );
+
     formData.append('patch', jsonBlob);
 
     if (selectedFile) {
       formData.append('file', selectedFile, selectedFile.name);
-    };
-  
-    axios.patch(`${process.env.REACT_APP_API_URL}/api/members/${memberId}`, 
-    formData, 
-      {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-          Authorization: localStorage.getItem('access_token'),
+    }
+
+    axios
+      .patch(
+        `${process.env.REACT_APP_API_URL}/api/members/${memberId}`,
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+            Authorization: localStorage.getItem('access_token')
+          }
         }
-      }
-    )
+      )
       .then((response) => {
         setIsEditClick(false);
+        dispatch(setUser(response.data.data));
       })
       .catch((error) => {
         console.error('프로필 저장 요청 실패:', error);
@@ -113,58 +127,67 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
     window.location.reload();
   };
 
-
   // 회원탈퇴 로직
   const handleRemoveClick = () => {
-    const result = window.confirm('회원탈퇴를 선택하시면 모든 계정 정보가 영구적으로 삭제됩니다.\n계속 진행하시겠습니까?');
-  
+    const result = window.confirm(
+      '회원탈퇴를 선택하시면 모든 계정 정보가 영구적으로 삭제됩니다.\n계속 진행하시겠습니까?'
+    );
+
     if (result) {
       const token = localStorage.getItem('access_token');
       axios
-      .delete(
-        `${process.env.REACT_APP_API_URL}/api/members/${memberId}`,
-        {
+        .delete(`${process.env.REACT_APP_API_URL}/api/members/${memberId}`, {
           headers: {
             Authorization: `${token}`
           }
-        }
-      )
-      .then((response) => {
-        alert('회원탈퇴가 완료되었습니다. 이용해주셔서 감사합니다.');
-        localStorage.removeItem('user');
-        localStorage.removeItem('access_token');
-        localStorage.removeItem('refresh_token');
-        navigate(`${PATH_URL.HOME}`);
-        window.location.reload();
-      })
-      .catch((error) => {
-        console.error('언팔로우 요청 실패:', error);
-      });
+        })
+        .then((response) => {
+          alert('회원탈퇴가 완료되었습니다. 이용해주셔서 감사합니다.');
+          localStorage.removeItem('user');
+          localStorage.removeItem('access_token');
+          localStorage.removeItem('refresh_token');
+          navigate(`${PATH_URL.HOME}`);
+          window.location.reload();
+        })
+        .catch((error) => {
+          console.error('언팔로우 요청 실패:', error);
+        });
     } else {
       return;
-    };
+    }
   };
 
   return (
     <StyledEditWrapper>
-        <StyledText>이미지 수정:</StyledText>
+      <StyledText>이미지 수정:</StyledText>
       <StyledEditImg>
-        <input type="file" onChange={handleUploadImg} style={{ display: 'none' }} ref={fileInputRef} />
-        <FiEdit onClick={() => { (document.querySelector('input[type="file"]') as HTMLInputElement)?.click(); }} />
-      </StyledEditImg>
-        <UserProfileImg isUserImg={isUserImg} />
-        닉네임 수정:
-        <Input 
-          placeholder="닉네임 수정" 
-          prefix={<UserOutlined />}
-          style={{ width: '200px' }} 
-          value={isUserName}
-          onChange={handleNicknameChange}
+        <input
+          type='file'
+          onChange={handleUploadImg}
+          style={{ display: 'none' }}
+          ref={fileInputRef}
         />
-        소개글 수정:
+        <FiEdit
+          onClick={() => {
+            (
+              document.querySelector('input[type="file"]') as HTMLInputElement
+            )?.click();
+          }}
+        />
+      </StyledEditImg>
+      <UserProfileImg isUserImg={isUserImg} />
+      닉네임 수정:
+      <Input
+        placeholder='닉네임 수정'
+        prefix={<UserOutlined />}
+        style={{ width: '200px' }}
+        value={isUserName}
+        onChange={handleNicknameChange}
+      />
+      소개글 수정:
       <StyledEditAboutMe>
-        <TextArea 
-          placeholder="소개글을 작성해주세요." 
+        <TextArea
+          placeholder='소개글을 작성해주세요.'
           maxLength={100}
           style={{ height: '100px', width: '300px' }}
           value={isEditAboutMe}
@@ -172,18 +195,16 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
           autoSize={{ minRows: 3, maxRows: 5 }}
         />
       </StyledEditAboutMe>
-      <StyledActionContain>  
-        <CreateChannelButton 
-          text={'프로필 저장하기'} 
+      <StyledActionContain>
+        <CreateChannelButton
+          text={'프로필 저장하기'}
           onClick={handleSubmitEditClick}
         />
         <StyledCancleContain>
           <CiCircleRemove onClick={handleCancleClick} />
         </StyledCancleContain>
       </StyledActionContain>
-      <StyledRemoveBtn onClick={handleRemoveClick} >
-        회원탈퇴
-      </StyledRemoveBtn>
+      <StyledRemoveBtn onClick={handleRemoveClick}>회원탈퇴</StyledRemoveBtn>
     </StyledEditWrapper>
   );
 };
