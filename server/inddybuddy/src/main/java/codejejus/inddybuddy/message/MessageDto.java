dd4020086543d5b11e17fe846203766baf1207e8
zqwz <zqwz@naver.com>
2023-05-23 16:51:31 +0900

    feat : 회원 정보 Dto 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/message/MessageDto.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/message/MessageDto.java
@@ -1,5 +1,6 @@
 package codejejus.inddybuddy.message;
 
+import codejejus.inddybuddy.member.entity.Member;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,9 +19,21 @@ public class MessageDto {
     @Getter
     static class Response {
 
-        private Long senderId;
-        private String content;
+        private final Long senderId;
+        private final String content;
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss", timezone = "Asia/Seoul")
-        private LocalDateTime createdAt;
+        private final LocalDateTime createdAt;
+    }
+
+    @Getter
+    public static class MemberResponse {
+
+        private final Member sender;
+        private final Member receiver;
+
+        public MemberResponse(Message message) {
+            this.sender = message.getSender();
+            this.receiver = message.getReceiver();
+        }
     }
 }
