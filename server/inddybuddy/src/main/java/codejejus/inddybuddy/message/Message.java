9d38291cf661bdbbf4662a9359597d4806320f9a
zqwz <zqwz@naver.com>
2023-05-23 16:51:18 +0900

    feat : 메세지 Set 중복 검사 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/message/Message.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/message/Message.java
@@ -7,6 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -30,5 +31,18 @@ public class Message extends Timestamped {
         this.sender = sender;
         this.receiver = receiver;
         this.content = content;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sender, receiver);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Message message = (Message) o;
+        return Objects.equals(sender, message.sender) && Objects.equals(receiver, message.receiver);
     }
 }
