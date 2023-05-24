e96f9207ce4e8c6f721e29b05d6254ab980cf869
crowwan <crowwan@naver.com>
2023-05-24 17:04:32 +0900

    :sparkles: feat: 팔로우한 유저 컴포넌트 기능 구현 완료

--- client/src/components/GameFollower/FollowerItem.tsx
+++ client/src/components/GameFollower/FollowerItem.tsx
@@ -1,7 +1,179 @@
-import React from 'react';
+import styled from 'styled-components';
+import { User } from '../../types/dataTypes';
+import UserProfileImg from '../UserInfo/UserProfileImg';
+import { StyledFollowed } from '../UserInfo/UserProfileName';
+import { TbMessages } from 'react-icons/tb';
+import { StyledItemWrapper } from '../CategoryGames/GameItem';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../../store/store';
+import { useNavigate } from 'react-router-dom';
+import PATH_URL from '../../constants/pathUrl';
+import { deleteData, postData } from '../../api/apiCollection';
+import { useState } from 'react';
+import { startChat } from '../../slice/chatSlice';
 
-const FollowerItem = () => {
-  return <div>FollowerItem</div>;
+const FollowerItem = (
+  props: User & {
+    followerCount: number;
+    followingCount: number;
+    isFollowed: boolean;
+  }
+) => {
+  const navigate = useNavigate();
+  const user = useSelector((s: RootState) => s.user);
+  const [isFollowed, setIsFollowed] = useState(props.isFollowed);
+  const dispatch = useDispatch();
+
+  const onFollowBtnClickHandler = () => {
+    if (isFollowed) {
+      deleteData(
+        `${process.env.REACT_APP_API_URL}/api/members/${props.memberId}/unfollow`,
+        {
+          headers: {
+            Authorization: localStorage.getItem('access_token')
+          }
+        },
+        () => {
+          setIsFollowed(false);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    } else {
+      postData(
+        `${process.env.REACT_APP_API_URL}/api/members/${props.memberId}/follow`,
+        {},
+        {
+          headers: {
+            Authorization: localStorage.getItem('access_token')
+          }
+        },
+        () => {
+          setIsFollowed(true);
+        },
+        (err) => {
+          console.error(err);
+        }
+      );
+    }
+  };
+
+  const onUserClickHandler = (memberId: number) => () => {
+    navigate(`${PATH_URL.USER_INFO}${memberId}`);
+  };
+
+  const onMessageClickHandler = () => {
+    dispatch(
+      startChat({
+        memberId: props.memberId,
+        imageUrl: props.imageUrl,
+        userName: props.userName
+      })
+    );
+  };
+
+  return (
+    <StyledWrapper>
+      <StyledContain onClick={onUserClickHandler(props.memberId)}>
+        <UserProfileImg isUserImg={props.imageUrl} />
+        <StyledUserName>
+          {props.userName.length >= 20 ? '*삭제된 계정*' : props.userName}
+        </StyledUserName>
+        <StyledFollowed>
+          <p>팔로워: {props.followerCount}</p>
+          <p>팔로잉: {props.followingCount}</p>
+        </StyledFollowed>
+      </StyledContain>
+      {props.userName.length < 11 &&
+        (user.memberId !== props.memberId ? (
+          <StyledRow>
+            <StyledFollowButton onClick={onFollowBtnClickHandler}>
+              {isFollowed ? '팔로우 취소' : '팔로우'}
+            </StyledFollowButton>
+            <StyledMessageContain>
+              <TbMessages onClick={onMessageClickHandler} />
+            </StyledMessageContain>
+          </StyledRow>
+        ) : (
+          <StyledRow>
+            <StyledFollowButton onClick={onUserClickHandler(user.memberId)}>
+              {'내 프로필보기'}
+            </StyledFollowButton>
+          </StyledRow>
+        ))}
+    </StyledWrapper>
+  );
 };
 
 export default FollowerItem;
+
+const StyledWrapper = styled(StyledItemWrapper)`
+  display: flex;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
+  border-radius: 15px;
+  justify-content: center;
+  align-items: center;
+  padding-top: 20px;
+  padding-bottom: 10px;
+  cursor: default;
+  &:hover {
+    color: #000;
+  }
+`;
+
+const StyledContain = styled.div`
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 200px;
+`;
+
+const StyledUserName = styled.p`
+  padding-top: 10px;
+  font-size: 14px;
+  padding: 0px 20px;
+  margin-top: 15px;
+  width: 100%;
+  word-break: keep-all;
+  overflow-wrap: break-word;
+  text-align: center;
+`;
+
+const StyledFollowButton = styled.div`
+  font-size: 15px;
+  padding: 10px 25px;
+  background-color: var(--cyan-light-700);
+  border-radius: 15px;
+  color: var(--cyan-light-100);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex: 1 0 0;
+  justify-content: center;
+
+  cursor: pointer;
+  &:hover {
+    background-color: var(--button-inactive-hover-color);
+  }
+`;
+
+const StyledMessageContain = styled.div`
+  color: var(--cyan-dark-800);
+  font-size: 35px;
+  cursor: pointer;
+  &:hover {
+    color: var(--button-inactive-color);
+  }
+`;
+
+const StyledRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  /* justify-content: space-between; */
+  gap: 1rem;
+  width: 100%;
+  padding: 1rem;
+`;
