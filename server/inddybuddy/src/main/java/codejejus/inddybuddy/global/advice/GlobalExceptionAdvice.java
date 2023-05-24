58f57382afbd3946f6c802cdbcaa61f5b4068d77
zqwz <zqwz@naver.com>
2023-05-24 15:36:50 +0900

    refactor : 유효성 검사 로직 변경

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/global/advice/GlobalExceptionAdvice.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/global/advice/GlobalExceptionAdvice.java
@@ -1,17 +1,14 @@
 package codejejus.inddybuddy.global.advice;
 
+import codejejus.inddybuddy.global.dto.ErrorFieldResponse;
 import codejejus.inddybuddy.global.dto.ErrorResponse;
 import codejejus.inddybuddy.global.exception.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
@@ -23,9 +20,8 @@ public class GlobalExceptionAdvice {
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<Map<String, String>> handleValidationException(BindingResult bindingResult) {
-        Map<String, String> errors = new HashMap<>();
-        bindingResult.getAllErrors().forEach(error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    public ResponseEntity<ErrorFieldResponse> handleValidationException(BindingResult bindingResult) {
+        ErrorFieldResponse errorFieldResponse = ErrorFieldResponse.of(bindingResult);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorFieldResponse);
     }
 }
