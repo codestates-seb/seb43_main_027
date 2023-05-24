f9b44863034d72de89c078757b1b14e59aec6b77
crowwan <crowwan@naver.com>
2023-05-24 17:04:53 +0900

    :sparkles: feat: 게임 팔로우한 유저 타이틀 컴포넌트 생성

--- client/src/components/GameFollower/Title.tsx
+++ client/src/components/GameFollower/Title.tsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 
-const Title = ({ gameName }: { gameName: string }) => {
-  return <StyledTitle>{gameName}을 팔로우한 유저</StyledTitle>;
+const Title = () => {
+  return <StyledTitle>이 게임을 팔로우한 유저</StyledTitle>;
 };
 
 export default Title;
 
 const StyledTitle = styled.h3`
   font-size: 2rem;
-  padding: 0.5rem;
+  padding: 2rem 0.5rem 0;
 `;
