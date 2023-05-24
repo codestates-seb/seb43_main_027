02b1ab0cf74eb91b67597ec82a39c2e764aa26f6
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-05-25 00:54:05 +0900

    :sparkles: feat: 비밀번호 재설정 기능 구현

--- client/src/components/UserInfo/UserEditInfo.tsx
+++ client/src/components/UserInfo/UserEditInfo.tsx
@@ -9,10 +9,10 @@ import { CiCircleRemove } from 'react-icons/ci';
 import { FiEdit } from 'react-icons/fi';
 import { UserInfoProps } from '../../types/propsTypes';
 import PATH_URL from '../../constants/pathUrl';
-import { UserOutlined } from '@ant-design/icons';
+import { UserOutlined, EyeInvisibleOutlined, EyeTwoTone } from '@ant-design/icons';
 import { Input } from 'antd';
 import { useDispatch } from 'react-redux';
-import { setUser } from '../../slice/userSlice';
+import { setUser, clearUser } from '../../slice/userSlice';
 
 const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
   const { memberId } = useParams();
@@ -23,11 +23,17 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
   const [isUserName, setIsUserName] = useState<string>('');
   const [isUserEmail, setIsUserEmail] = useState<string>('');
   const [isEditAboutMe, setIsEditAboutMe] = useState<string>('');
+  const [ editPassword, setEditPassword ] = useState<string>('');
+  const [ vaildPassword, setVaildPassword ] = useState<string>('');
+  const [error, setError] = useState('');
+  const [remindError, setRemindError] = useState('');
+  const [isOpenNewInput, setIsOpenNewInput] = useState<boolean>(false);
 
   const fileInputRef = useRef<HTMLInputElement>(null);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
-
   const { TextArea } = Input;
+
+  const passwordValidate = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+\-={}[\]|:;"'<>,.?/]).{8,16}$/;
 
   useEffect(() => {
     const fetchUserData = async () => {
@@ -47,8 +53,6 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
           `${process.env.REACT_APP_API_URL}/api/members/${memberId}/profile`
         );
         const fetchedData = res.data.data;
-
-        console.log(fetchedData);
 
         setIsUserImg(fetchedData.imageUrl);
         setIsUserName(fetchedData.userName);
@@ -82,6 +86,37 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
 
   const handleAboutChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
     setIsEditAboutMe(event.target.value);
+  };
+
+  const handlePasswordChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    if (!passwordValidate.test(value)) {
+      setError('비밀번호는 영문자, 숫자, 특수문자 조합의 8-16자여야 합니다.');
+      setEditPassword(value);
+      return;
+    }
+  
+    setEditPassword(value);
+    setError('');
+
+    if (value !== vaildPassword) {
+      setRemindError('비밀번호가 일치하지 않습니다.');
+    } else {
+      setRemindError('');
+    }
+  };
+
+  const handleValidChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const { value } = event.target;
+
+    if (editPassword !== value) {
+      setRemindError('비밀번호가 일치하지 않습니다.');
+    } else {
+      setRemindError('');
+    }
+
+    setVaildPassword(value);
   };
 
   // 프로필 수정 로직
@@ -127,6 +162,56 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
   // 프로필 수정 취소 로직
   const handleCancleClick = () => {
     window.location.reload();
+  };
+
+  // 비밀번호 재설정 로직
+  const handlePasswordClick = () => {
+    setIsOpenNewInput(true);
+  };
+
+  const handleCanclePassword = () => {
+    setIsOpenNewInput(false);
+  };
+
+  const handleSubmitNewPassword = () => {
+    if (!error && !remindError) {
+      console.log('비밀번호 변경됨:', editPassword);
+      const formData = new FormData();
+      const jsonBlob = new Blob(
+        [
+          JSON.stringify({
+            password: editPassword
+          })
+        ],
+        { type: 'application/json' }
+      );
+      formData.append('patch', jsonBlob);
+  
+      axios
+        .patch(
+          `${process.env.REACT_APP_API_URL}/api/members/${memberId}`,
+          formData,
+          {
+            headers: {
+              'Content-Type': 'multipart/form-data',
+              Authorization: localStorage.getItem('access_token')
+            }
+          }
+        )
+        .then((response) => {
+          alert('비밀번호 변경 성공!');
+          localStorage.removeItem('access_token');
+          localStorage.removeItem('refresh_token');
+          localStorage.removeItem('user');
+          dispatch(clearUser());
+          navigate(`${PATH_URL.LOGIN}`);
+        })
+        .catch((error) => {
+          console.error('프로필 저장 요청 실패:', error);
+        });
+    } else {
+      return;
+    }
   };
 
   // 회원탈퇴 로직
@@ -207,6 +292,42 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
           <CiCircleRemove onClick={handleCancleClick} />
         </StyledCancleContain>
       </StyledActionContain>
+      { 
+        isOpenNewInput ? (
+          <StyledInputForm>
+            <p>재설정 비밀번호:</p>
+            {error.length > 0 && <StyledError>{error}</StyledError>}
+            <Input.Password
+              placeholder="input password"
+              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              onChange={handlePasswordChange}
+            />
+            <p>재확인 비밀번호:</p>
+            {remindError.length > 0 && <StyledError>{remindError}</StyledError>}
+            <Input.Password
+              placeholder="input password"
+              iconRender={(visible) => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+              onChange={handleValidChange}
+            />
+          </StyledInputForm>
+        ) : null
+      }
+      <StyledNewPassword
+      >
+        {
+          isOpenNewInput ? (
+          <StyledSubmitBtn onClick={handleSubmitNewPassword}>
+            비밀번호 변경
+          </StyledSubmitBtn>)
+        : (
+          <StyledSubmitBtn 
+            onClick={handlePasswordClick}
+          >
+            비밀번호 재설정
+          </StyledSubmitBtn>) 
+        }
+        {isOpenNewInput && <StyledCancleBtn onClick={handleCanclePassword}>변경취소</StyledCancleBtn>}
+      </StyledNewPassword>
       <StyledRemoveBtn onClick={handleRemoveClick}>회원탈퇴</StyledRemoveBtn>
     </StyledEditWrapper>
   );
@@ -249,8 +370,24 @@ const StyledCancleContain = styled.div`
   }
 `;
 
+const StyledNewPassword = styled.div`
+  margin-top: 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap:20px;
+`;
+
+const StyledSubmitBtn = styled.div`
+  cursor: pointer;
+  color: var(--category-tag-bg-default);
+  &:hover {
+    color: var(--cyan-dark-600);
+  }
+`;
+
 const StyledRemoveBtn = styled.div`
-  margin-top: 20px;
+  margin-top: 10px;
   font-size: 14px;
   color: var(--default-text-color);
   cursor: pointer;
@@ -272,4 +409,31 @@ const StyledEditImg = styled.div`
 
 const StyledEmailText = styled.p`
   color: var(--cyan-light-700);
+`;
+
+const StyledInputForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  font-size: 12px;
+  color: var(--cyan-light-700);
+`;
+
+const StyledCancleBtn = styled.button`
+  border-style: none;
+  font-size: 12px;
+  font-weight: 700;
+  color: var(--category-tag-color-default);
+  background-color: var(--sub-text-color);
+  border-radius: 8px;
+  padding: 5px 8px;
+  &:hover {
+    background-color: var(--button-hover-color);
+  }
+`;
+
+const StyledError = styled.p`
+  color: var(--category-tag-color-2);
 `;
