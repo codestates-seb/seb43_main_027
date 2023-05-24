91f192b4794acf98a4d9a78256531453fc9091c1
zqwz <zqwz@naver.com>
2023-05-24 17:44:26 +0900

    refactor : 게시글 조회 N + 1 해결

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/post/PostRepository.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/post/PostRepository.java
@@ -10,18 +10,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
 
-    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     Page<Post> findAllByContentContainingOrTitleContaining(String content, String title, Pageable pageable);
 
-    @EntityGraph(attributePaths = {"game", "member"}, type = EntityGraph.EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     Page<Post> findAllByGame(Game game, Pageable pageable);
 
-    @EntityGraph(attributePaths = {"game", "member"}, type = EntityGraph.EntityGraphType.FETCH)
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraph.EntityGraphType.FETCH)
     Page<Post> findAllByGameAndPostTag(Game game, Post.PostTag postTag, PageRequest pageRequest);
 
-    @EntityGraph(attributePaths = {"game", "member"})
     Page<Post> findAllByMemberAndPostTag(Member member, Post.PostTag postTag, Pageable pageable);
 
-    @EntityGraph(attributePaths = {"game", "member"})
     Page<Post> findAllByMember(Member member, Pageable pageable);
 }
