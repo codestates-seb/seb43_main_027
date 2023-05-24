b090294d8af71e4ee40a849c5f113d84f9c6e898
zqwz <zqwz@naver.com>
2023-05-24 14:38:44 +0900

    refactor : URL 요청 권한 수정

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/global/config/SecurityConfig.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/global/config/SecurityConfig.java
@@ -64,12 +64,12 @@ public class SecurityConfig {
 
                 .and()
                 .authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/api/**").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("USER", "ADMIN")
-                .antMatchers(HttpMethod.DELETE, "/api/**").hasAnyRole("USER", "ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/oauth/**").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/members/signup").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/auth/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/api/**").hasAnyRole("USER", "ADMIN")
+                .antMatchers(HttpMethod.DELETE, "/api/**").hasAnyRole("USER", "ADMIN")
                 .anyRequest().permitAll()
 
                 .and()
