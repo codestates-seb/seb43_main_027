c60a4124fde47f41e4bdb226547236375f9cef31
UseonJ <naul630@gmail.com>
2023-05-24 20:50:29 +0900

    :sparkles: feat: 검색결과 유저 탭 구현

--- client/src/components/Search/SearchUser.tsx
+++ client/src/components/Search/SearchUser.tsx
@@ -2,12 +2,26 @@ import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 
 // import { type UserType } from '../../types/dataTypes';
-
+import { type PageInfoType } from '../../types/dataTypes';
 import GameItem from '../CategoryGames/GameItem';
 
 interface SearchUserType {
   isSelectTab: '전체' | '유저';
-  searchUserInfo: any[];
+  searchUserInfo: SearchUserInfo;
+}
+
+export interface UserType {
+  memberId: number;
+  email: string;
+  userName: string;
+  imageUrl: string;
+  followerCount: number;
+  followingCount: number;
+}
+
+export interface SearchUserInfo {
+  data: UserType[];
+  pageInfo: PageInfoType;
 }
 
 interface SearchUserWrapperType {
@@ -15,9 +29,10 @@ interface SearchUserWrapperType {
 }
 
 const SearchUser = ({ isSelectTab, searchUserInfo }: SearchUserType) => {
+  console.log(searchUserInfo);
   return (
     <StyledSearchUserWrapper isPreview={isSelectTab === '전체' ? true : false}>
-      {searchUserInfo.length > 0 ? (
+      {searchUserInfo.data.length > 0 && searchUserInfo.data[0].memberId > 0 ? (
         isSelectTab === '전체' ? (
           <div>전체</div>
         ) : (
@@ -34,7 +49,8 @@ export default SearchUser;
 
 const StyledSearchUserWrapper = styled.div<SearchUserWrapperType>`
   display: flex;
-  flex-direction: ${(p) => (p.isPreview ? 'row' : 'column')};
+
+  ${(p) => (p.isPreview ? 'height:20%; align-items:center;' : 'height: 80%;')};
 `;
 
 const StyledEmptyItem = styled.div`
