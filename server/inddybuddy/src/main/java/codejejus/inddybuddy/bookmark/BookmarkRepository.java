46c241ec00afbd7b6de0e5d384460c44c130d03d
zqwz <zqwz@naver.com>
2023-05-24 17:46:20 +0900

    refactor : 북마크 게시글 조회 N + 1 해결

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/bookmark/BookmarkRepository.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/bookmark/BookmarkRepository.java
@@ -4,6 +4,7 @@ import codejejus.inddybuddy.member.entity.Member;
 import codejejus.inddybuddy.post.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -14,7 +15,9 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     Optional<Bookmark> findByMemberAndPost(Member member, Post post);
 
+    @EntityGraph(attributePaths = {"post", "post.member"})
     Page<Bookmark> findAllByMember(Member member, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"post", "post.member"})
     Page<Bookmark> findAllByMemberAndPost_PostTag(Member member, Post.PostTag postTag, Pageable pageable);
 }
