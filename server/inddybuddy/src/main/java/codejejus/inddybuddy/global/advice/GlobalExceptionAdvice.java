4e7a18d2784f6a918f6a20300a7e3e3d791fccb4
zqwz <zqwz@naver.com>
2023-05-24 15:13:26 +0900

    feat : 유효성 검사 오류 핸들러 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/global/advice/GlobalExceptionAdvice.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/global/advice/GlobalExceptionAdvice.java
@@ -4,8 +4,14 @@ import codejejus.inddybuddy.global.dto.ErrorResponse;
 import codejejus.inddybuddy.global.exception.CustomException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionAdvice {
@@ -14,5 +20,12 @@ public class GlobalExceptionAdvice {
     public ResponseEntity<ErrorResponse> handleCustomException(CustomException customException) {
         ErrorResponse errorResponse = ErrorResponse.of(customException.getExceptionCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(customException.getExceptionCode().getCode()));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleValidationException(BindingResult bindingResult) {
+        Map<String, String> errors = new HashMap<>();
+        bindingResult.getAllErrors().forEach(error -> errors.put(((FieldError) error).getField(), error.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 }
