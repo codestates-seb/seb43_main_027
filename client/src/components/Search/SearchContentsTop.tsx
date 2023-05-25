02a3b87203e1d166c3a577abfb55f7067f0c7490
UseonJ <naul630@gmail.com>
2023-05-25 12:14:25 +0900

    :sparkles: feat: 검색 각 섹션 위쪽 탭 부분 추가

---
+++ client/src/components/Search/SearchContentsTop.tsx
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import ButtonEl from '../elements/Button';
+
+const SearchContentsTop = () => {
+  const movetoContentsTap = (e: any) => {
+    e.preventDefault();
+    console.log('dd');
+  };
+  return (
+    <StyledWrapper>
+      <StyledTitle>게시글</StyledTitle>
+      <StyledBlock />
+      <StyledTapButton onClick={movetoContentsTap}>전체보기</StyledTapButton>
+    </StyledWrapper>
+  );
+};
+
+export default SearchContentsTop;
+
+const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  button {
+    font-family: 'yg-jalnan';
+  }
+`;
+
+const StyledTitle = styled.span`
+  display: flex;
+  color: black;
+  font-size: 3rem;
+  margin: 3rem 0 0 5rem;
+`;
+
+const StyledBlock = styled.div`
+  display: flex;
+  width: 70%;
+`;
+
+const StyledTapButton = ButtonEl({
+  margin: '2rem',
+  fontSize: '2rem',
+  hoverBg: 'var(--cyan-dark-500)'
+});
