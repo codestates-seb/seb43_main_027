077d08d04f650ceb5d0752ddf19cd9fb9fe578f2
UseonJ <naul630@gmail.com>
2023-05-25 12:14:43 +0900

    :sparkles: feat: 유저검색부분 추가

--- client/src/components/Search/SearchUser.tsx
+++ client/src/components/Search/SearchUser.tsx
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 // import { type UserType } from '../../types/dataTypes';
 import { type PageInfoType } from '../../types/dataTypes';
-import GameItem from '../CategoryGames/GameItem';
+import UserItem from '../UserInfo/UserItem';
+import SearchUserTop from './SearchUserTop';
 
 interface SearchUserType {
   isSelectTab: '전체' | '유저';
@@ -30,17 +32,77 @@ interface SearchUserWrapperType {
 
 const SearchUser = ({ isSelectTab, searchUserInfo }: SearchUserType) => {
   console.log(searchUserInfo);
+  const [isMyFollowingList, setIsMyFollowingList] = useState<number[]>([]);
+
+  const isFollowingIdIncluded = (memberId: number) => {
+    return isMyFollowingList.includes(memberId);
+  };
+  let loginedId = -1;
+  const token = localStorage.getItem('access_token');
+  useEffect(() => {
+    const user = localStorage.getItem('user');
+    if (user) {
+      loginedId = Number(JSON.parse(user).memberId);
+    }
+  }, [token]);
+
+  useEffect(() => {
+    if (loginedId !== -1) {
+      const MyFollowingData = async () => {
+        try {
+          const res = await axios.get(
+            `${process.env.REACT_APP_API_URL}/api/members/${loginedId}/following`
+          );
+          const followData = res.data.data;
+          const followingIdList = followData.map((item: any) => item.memberId);
+          setIsMyFollowingList([...followingIdList]);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      MyFollowingData();
+    }
+  }, [searchUserInfo, loginedId]);
+
   return (
     <StyledSearchUserWrapper isPreview={isSelectTab === '전체' ? true : false}>
-      {searchUserInfo.data.length > 0 && searchUserInfo.data[0].memberId > 0 ? (
-        isSelectTab === '전체' ? (
-          <div>전체</div>
+      {isSelectTab === '전체' && <SearchUserTop />}
+      <StyledProfileContainer isPreview={isSelectTab === '전체' ? true : false}>
+        {searchUserInfo.data.length > 0 &&
+        searchUserInfo.data[0].memberId > 0 ? (
+          isSelectTab === '전체' ? (
+            searchUserInfo.data.slice(0, 5).map((item, index) => {
+              return (
+                <UserItem
+                  key={index}
+                  imageUrl={item.imageUrl}
+                  userName={item.userName}
+                  followerCount={item.followerCount}
+                  followingCount={item.followingCount}
+                  memberId={String(item.memberId)}
+                  isFollowingIdIncluded={isFollowingIdIncluded(item.memberId)}
+                />
+              );
+            })
+          ) : (
+            searchUserInfo.data.map((item, index) => {
+              return (
+                <UserItem
+                  key={index}
+                  imageUrl={item.imageUrl}
+                  userName={item.userName}
+                  followerCount={item.followerCount}
+                  followingCount={item.followingCount}
+                  memberId={String(item.memberId)}
+                  isFollowingIdIncluded={isFollowingIdIncluded(item.memberId)}
+                />
+              );
+            })
+          )
         ) : (
-          <div>item</div>
-        )
-      ) : (
-        <StyledEmptyItem>검색 결과가 없습니다.</StyledEmptyItem>
-      )}
+          <StyledEmptyItem>검색 결과가 없습니다.</StyledEmptyItem>
+        )}
+      </StyledProfileContainer>
     </StyledSearchUserWrapper>
   );
 };
@@ -49,8 +111,23 @@ export default SearchUser;
 
 const StyledSearchUserWrapper = styled.div<SearchUserWrapperType>`
   display: flex;
+  flex-direction: column;
+  background-color: #cfecee;
+  margin: 3rem;
+  border-radius: 25px;
+  ${(p) => (p.isPreview ? 'height:30%; align-items:center;' : 'height: 80%;')};
+`;
 
-  ${(p) => (p.isPreview ? 'height:20%; align-items:center;' : 'height: 80%;')};
+const StyledProfileContainer = styled.div<SearchUserWrapperType>`
+  display: flex;
+  justify-content: flex-start;
+  ${({ isPreview }) =>
+    isPreview ? 'padding: 0 3rem 2rem 3rem;' : 'padding: 3rem;'}
+  gap: 2rem;
+  width: 100%;
+  > div {
+    background-color: white;
+  }
 `;
 
 const StyledEmptyItem = styled.div`
