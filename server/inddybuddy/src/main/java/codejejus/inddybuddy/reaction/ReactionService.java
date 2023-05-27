bfb9db57faf6e2230d82fba914bed781ae9844f4
zqwz <zqwz@naver.com>
2023-05-27 21:39:22 +0900

    feat : 게시글 좋아요 알림 기능 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/reaction/ReactionService.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/reaction/ReactionService.java
@@ -5,15 +5,20 @@ import codejejus.inddybuddy.global.exception.ExceptionCode;
 import codejejus.inddybuddy.member.entity.Member;
 import codejejus.inddybuddy.member.entity.MemberPrincipal;
 import codejejus.inddybuddy.member.service.MemberService;
+import codejejus.inddybuddy.notification.Alarm;
+import codejejus.inddybuddy.notification.AlarmEvent;
+import codejejus.inddybuddy.notification.AlarmType;
 import codejejus.inddybuddy.post.Post;
-import codejejus.inddybuddy.post.PostRepository;
 import codejejus.inddybuddy.post.PostService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ReactionService {
 
@@ -21,14 +26,26 @@ public class ReactionService {
     private final PostService postService;
     private final ReactionMapper reactionMapper;
     private final MemberService memberService;
+    private final ApplicationEventPublisher eventPublisher;
 
     public ReactionDto.Response createReaction(MemberPrincipal memberPrincipal, ReactionDto.Request request, Long postId) {
         Member member = memberService.findMember(memberPrincipal.getMember().getMemberId());
         Post post = postService.findVerifidPost(postId);
+        Member receiver = post.getMember();
         Reaction reaction = reactionMapper.dtoToEntity(request);
         reaction.update(member, post);
         verifyExistReaction(member, post);
         Reaction save = reactionRepository.save(reaction);
+
+        String content = member.getUsername() + "님이 회원님의 게시물에 " + request.getReactionStatus() + " 하였습니다.";
+        Alarm alarm = Alarm.builder()
+                .alarmType(AlarmType.POST_LIKE)
+                .post(post)
+                .sender(member)
+                .receiver(receiver)
+                .content(content)
+                .build();
+        eventPublisher.publishEvent(new AlarmEvent(this, alarm));
         return reactionMapper.entityToResponse(save);
     }
 
