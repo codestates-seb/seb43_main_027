74cc6e9fb82413c2151e010309a0963953f1596a
zqwz <zqwz@naver.com>
2023-05-27 21:38:47 +0900

    feat : 메시지 알림 기능 추가

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/message/MessageService.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/message/MessageService.java
@@ -6,12 +6,17 @@ import codejejus.inddybuddy.global.exception.ExceptionCode;
 import codejejus.inddybuddy.member.entity.Member;
 import codejejus.inddybuddy.member.entity.MemberPrincipal;
 import codejejus.inddybuddy.member.service.MemberService;
+import codejejus.inddybuddy.notification.Alarm;
+import codejejus.inddybuddy.notification.AlarmEvent;
+import codejejus.inddybuddy.notification.AlarmType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
@@ -19,12 +24,14 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 @Slf4j
 public class MessageService {
 
     private final MessageRepository messageRepository;
     private final MemberService memberService;
     private final MessageMapper messageMapper;
+    private final ApplicationEventPublisher eventPublisher;
 
     public void sendMessage(MessageDto.Request request, MemberPrincipal memberPrincipal, Long receiverId) {
         Member receiver = memberService.findMember(receiverId);
@@ -33,6 +40,15 @@ public class MessageService {
 
         Message message = messageMapper.toEntity(request, sender, receiver);
         messageRepository.save(message);
+
+        String content = sender.getUsername() + "님이 [" + request.getContent() + "] 메세지를 보냈습니다.";
+        Alarm alarm = Alarm.builder()
+                .receiver(receiver)
+                .sender(sender)
+                .alarmType(AlarmType.MESSAGE)
+                .content(content)
+                .build();
+        eventPublisher.publishEvent(new AlarmEvent(this, alarm));
     }
 
     public Page<MessageDto.Response> findSendMessages(MemberPrincipal memberPrincipal, Long receiverId, Pageable pageable) {
@@ -40,14 +56,15 @@ public class MessageService {
         Member sender = memberPrincipal.getMember();
         verifySender(receiver, sender);
 
-        Page<Message> messages = messageRepository
-                .findAllMessages(sender, receiver, PageRequest.of(pageable.getPageNumber() - 1, pageable.getPageSize(), Filter.getMatchedSort("new")));
+        Page<Message> messages = messageRepository.findAllMessages(sender, receiver, PageRequest.of(pageable.getPageNumber() - 1, pageable.getPageSize(), Filter.getMatchedSort("new")));
         return messageMapper.entityToResponses(messages);
     }
 
     public List<MessageDto.MemberResponse> findAllByMessageMembers(Member member) {
         Set<Message> messages = messageRepository.findAllByMessageMembers(member);
-        return messages.stream().map(MessageDto.MemberResponse::new).collect(Collectors.toList());
+        return messages.stream()
+                .map(MessageDto.MemberResponse::new)
+                .collect(Collectors.toList());
     }
 
     private void verifySender(Member receiver, Member sender) {
