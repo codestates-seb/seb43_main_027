46c58ff71bd887462883434656666ffec4c8717f
UseonJ <naul630@gmail.com>
2023-05-31 04:01:09 +0900

    :recycle: refactor: 검색어쿼리 통일 및 상속

--- client/src/pages/Search.tsx
+++ client/src/pages/Search.tsx
@@ -4,10 +4,7 @@ import axios from 'axios';
 
 import { useSearchParams } from 'react-router-dom';
 
-import SearchUser, {
-  SearchUserInfo,
-  UserType
-} from '../components/Search/SearchUser';
+import SearchUser, { SearchUserInfo } from '../components/Search/SearchUser';
 import SearchGame, { SearchGameInfo } from '../components/Search/SearchGame';
 import SearchContents, {
   SearchContentsInfo
@@ -16,7 +13,6 @@ import SearchPagination from '../components/Search/SearchPagination';
 import FilterTap from '../components/common/FilterTap';
 import { searchFilterTab } from '../data/filterTapList';
 import SkeletonComponent from '../components/common/SkeletonComponent';
-import { EmptyData, emptyData } from '../data/SearchResult';
 
 export interface SingleResponse {
   data: SearchGameInfo | SearchContentsInfo | SearchUserInfo;
@@ -26,7 +22,7 @@ export interface SingleResponse {
 const Search = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [searchParams] = useSearchParams();
-  const searchQuery = searchParams.get('q');
+  let searchQuery = searchParams.get('q');
   //  '전체' = all, 1 = user, 2 = game, 3 = contents
   const [isSelectTab, setIsSelectTab] = useState<string>(searchFilterTab[0]);
 
@@ -131,13 +127,13 @@ const Search = () => {
         filtered === 'members' ||
         filtered === 'posts'
       ) {
-        const slicedsearchQuery = searchQuery.slice(5);
+        // 원래 slicedsearchQuery라고 새로 변수만들어서 할당했는데 자식으로 넘겨주기에 두 종류이면 너무 넘겨주기가 어려워서 하나로 통일.
+        searchQuery = searchQuery.slice(5);
 
         const fetchFilteredData = async () => {
           try {
-            // Filter로 선택된 요청만 보냅니다.
             await axios(
-              `${process.env.REACT_APP_API_URL}/api/${filtered}/search?q=${slicedsearchQuery}`
+              `${process.env.REACT_APP_API_URL}/api/${filtered}/search?q=${searchQuery}&page=1&size=20`
             ).then((res) => {
               setIsLoading(false);
 
@@ -154,13 +150,13 @@ const Search = () => {
           try {
             // 여러 개의 POST 요청을 준비합니다.
             const getRequestUser = axios(
-              `${process.env.REACT_APP_API_URL}/api/members/search?q=${searchQuery}`
+              `${process.env.REACT_APP_API_URL}/api/members/search?q=${searchQuery}&page=1&size=20`
             );
             const getRequestGame = axios(
-              `${process.env.REACT_APP_API_URL}/api/games/search?q=${searchQuery}`
+              `${process.env.REACT_APP_API_URL}/api/games/search?q=${searchQuery}&page=1&size=20`
             );
             const getRequestContents = axios(
-              `${process.env.REACT_APP_API_URL}/api/posts/search?q=${searchQuery}`
+              `${process.env.REACT_APP_API_URL}/api/posts/search?q=${searchQuery}&page=1&size=20`
             );
 
             const responses = await Promise.all([
@@ -186,13 +182,13 @@ const Search = () => {
         try {
           // 여러 개의 POST 요청을 준비합니다.
           const getRequestUser = axios(
-            `${process.env.REACT_APP_API_URL}/api/members/search?q=${searchQuery}`
+            `${process.env.REACT_APP_API_URL}/api/members/search?q=${searchQuery}&page=1&size=20`
           );
           const getRequestGame = axios(
-            `${process.env.REACT_APP_API_URL}/api/games/search?q=${searchQuery}`
+            `${process.env.REACT_APP_API_URL}/api/games/search?q=${searchQuery}&page=1&size=20`
           );
           const getRequestContents = axios(
-            `${process.env.REACT_APP_API_URL}/api/posts/search?q=${searchQuery}`
+            `${process.env.REACT_APP_API_URL}/api/posts/search?q=${searchQuery}&page=1&size=20`
           );
 
           const responses = await Promise.all([
@@ -223,13 +219,21 @@ const Search = () => {
         (isLoading || !data[0] || !data[0].data ? (
           <SkeletonComponent />
         ) : (
-          <SearchUser isSelectTab={isSelectTab} searchUserInfo={data[0].data} />
+          <SearchUser
+            isSelectTab={isSelectTab}
+            searchUserInfo={data[0].data}
+            searchQuery={searchQuery}
+          />
         ))}
       {(isSelectTab === '전체' || isSelectTab === '게임') &&
         (isLoading || !data[1] || !data[1].data ? (
           <SkeletonComponent />
         ) : (
-          <SearchGame isSelectTab={isSelectTab} searchGameInfo={data[1].data} />
+          <SearchGame
+            isSelectTab={isSelectTab}
+            searchGameInfo={data[1].data}
+            searchQuery={searchQuery}
+          />
         ))}
       {(isSelectTab === '전체' || isSelectTab === '게시글') &&
         (isLoading || !data[2] || !data[2].data ? (
@@ -238,6 +242,7 @@ const Search = () => {
           <SearchContents
             isSelectTab={isSelectTab}
             searchContentsInfo={data[2].data}
+            searchQuery={searchQuery}
           />
         ))}
       {isSelectTab !== '전체' && <SearchPagination />}
