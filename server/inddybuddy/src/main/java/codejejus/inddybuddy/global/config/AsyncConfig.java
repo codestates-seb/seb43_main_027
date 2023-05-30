a2c31f856906fd80a48bd1595c38f2519578742d
zqwz <zqwz@naver.com>
2023-05-30 18:04:18 +0900

    feat : async config 설정

--- server/inddybuddy/src/main/java/codejejus/inddybuddy/global/config/AsyncConfig.java
+++ server/inddybuddy/src/main/java/codejejus/inddybuddy/global/config/AsyncConfig.java
@@ -1,9 +1,25 @@
 package codejejus.inddybuddy.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-@EnableAsync
+import java.util.concurrent.Executor;
+
 @Configuration
+@EnableAsync
 public class AsyncConfig {
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("ToGaether");
+        executor.initialize();
+        return executor;
+    }
+
 }
