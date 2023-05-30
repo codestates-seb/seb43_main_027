133045ffa9bda7b4f51f2fcd03d416b1abd6e9f9
UseonJ <naul630@gmail.com>
2023-05-31 04:00:19 +0900

    :sparkles: feat: 페이지네이션 구현

--- client/src/components/Search/SearchUser.tsx
+++ client/src/components/Search/SearchUser.tsx
@@ -1,15 +1,17 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router';
 
-// import { type UserType } from '../../types/dataTypes';
 import { type PageInfoType } from '../../types/dataTypes';
 import UserItem from '../UserInfo/UserItem';
 import SearchUserTop from './SearchUserTop';
+import Pagination from 'react-js-pagination';
 
 interface SearchUserType {
   isSelectTab: '전체' | '유저';
   searchUserInfo: SearchUserInfo;
+  searchQuery: string | null;
 }
 
 export interface UserType {
@@ -30,8 +32,58 @@ interface SearchUserWrapperType {
   isPreview: boolean;
 }
 
-const SearchUser = ({ isSelectTab, searchUserInfo }: SearchUserType) => {
+const SearchUser = ({
+  isSelectTab,
+  searchUserInfo,
+  searchQuery
+}: SearchUserType) => {
   const [isMyFollowingList, setIsMyFollowingList] = useState<number[]>([]);
+  const [reordered, setReordered] = useState<UserType[]>([]);
+  const [isPage, setPage] = useState<number>(1);
+  const [isSize, setSize] = useState<number>(20);
+  const [isToTalSize, setTotalSize] = useState<number>(0);
+  const navigate = useNavigate();
+  useEffect(() => {
+    setPage(1);
+  }, [isSelectTab]);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      if (searchUserInfo.data.length === 0) {
+        try {
+          if (searchQuery) {
+            setSize(searchUserInfo.pageInfo.size);
+            setTotalSize(searchUserInfo.pageInfo.totalSize);
+          } else {
+            const apiUrl = `${process.env.REACT_APP_API_URL}/api/members/search?q=&page=${isPage}&size=${isSize}`;
+
+            const res = await axios.get(apiUrl);
+            const userData = res.data.data.sort(
+              (a: { createdAt: string }, b: { createdAt: string }) =>
+                new Date(b.createdAt).getTime() -
+                new Date(a.createdAt).getTime()
+            );
+            const pageInfo = res.data.pageInfo;
+            setReordered([...userData]);
+            setSize(pageInfo.size);
+            setTotalSize(pageInfo.totalSize);
+            searchUserInfo.data = reordered;
+          }
+        } catch (error) {
+          return navigate('/error');
+        }
+      } else {
+        setSize(searchUserInfo.pageInfo.size);
+        setTotalSize(searchUserInfo.pageInfo.totalSize);
+      }
+    };
+    fetchUserData();
+  }, [searchUserInfo, isPage]);
+
+  const handlePageChange = (pageNumber: number) => {
+    setPage(pageNumber);
+    window.scrollTo(0, 0);
+  };
 
   const isFollowingIdIncluded = (memberId: number) => {
     return isMyFollowingList.includes(memberId);
@@ -47,19 +99,26 @@ const SearchUser = ({ isSelectTab, searchUserInfo }: SearchUserType) => {
 
   useEffect(() => {
     if (loginedId !== -1) {
-      const MyFollowingData = async () => {
-        try {
-          const res = await axios.get(
-            `${process.env.REACT_APP_API_URL}/api/members/${loginedId}/following`
-          );
-          const followData = res.data.data;
-          const followingIdList = followData.map((item: any) => item.memberId);
-          setIsMyFollowingList([...followingIdList]);
-        } catch (error) {
-          console.log(error);
-        }
-      };
-      MyFollowingData();
+      if (
+        searchUserInfo.data.length > 0 &&
+        searchUserInfo.data[0].memberId !== -1
+      ) {
+        const MyFollowingData = async () => {
+          try {
+            const res = await axios.get(
+              `${process.env.REACT_APP_API_URL}/api/members/${loginedId}/following`
+            );
+            const followData = res.data.data;
+            const followingIdList = followData.map(
+              (item: any) => item.memberId
+            );
+            setIsMyFollowingList([...followingIdList]);
+          } catch (error) {
+            console.log(error);
+          }
+        };
+        MyFollowingData();
+      }
     }
   }, [searchUserInfo, loginedId]);
 
@@ -102,6 +161,23 @@ const SearchUser = ({ isSelectTab, searchUserInfo }: SearchUserType) => {
           <StyledEmptyItem>검색 결과가 없습니다.</StyledEmptyItem>
         )}
       </StyledProfileContainer>
+      <StyledPaginationBox>
+        {searchUserInfo.data.length > 0 &&
+          searchUserInfo.data[0].memberId > 0 &&
+          isSelectTab !== '전체' && (
+            <StyledPagination>
+              <Pagination
+                activePage={isPage}
+                itemsCountPerPage={isSize}
+                totalItemsCount={isToTalSize}
+                pageRangeDisplayed={5}
+                prevPageText={'‹'}
+                nextPageText={'›'}
+                onChange={handlePageChange}
+              />
+            </StyledPagination>
+          )}
+      </StyledPaginationBox>
     </StyledSearchUserWrapper>
   );
 };
@@ -139,4 +215,13 @@ const StyledEmptyItem = styled.div`
   font-size: 18px;
   font-weight: 700;
   color: var(--default-text-color);
+`;
+
+const StyledPaginationBox = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledPagination = styled.div`
+  padding: 0px 30px 50px 30px;
 `;
