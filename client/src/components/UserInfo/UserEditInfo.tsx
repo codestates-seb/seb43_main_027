c1f8503bbfcfb803b711d9aeea4ccb0d36c9bb05
DESKTOP-PDMTIQO\hansu <fatholic5@gmail.com>
2023-05-30 19:46:53 +0900

    :recycle: refactor: 중복닉네임 안내문구 수정

--- client/src/components/UserInfo/UserEditInfo.tsx
+++ client/src/components/UserInfo/UserEditInfo.tsx
@@ -173,7 +173,7 @@ const UserEditInfo = ({ setIsEditClick }: UserInfoProps) => {
         dispatch(setUser(response.data.data));
       })
       .catch((error) => {
-        setNickNameError('중복되는 닉네임 입니다.');
+        setNickNameError('이미 사용중인 닉네임 입니다.');
       });
     } else {
       return;
