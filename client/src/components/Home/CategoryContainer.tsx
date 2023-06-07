649c9773bd1b6f2102a3842f44c9aeb198e9bb42
crowwan <crowwan@naver.com>
2023-06-07 21:09:45 +0900

    :recycle: refactor: 카테고리 카드 보여지는 컴포넌트 추가로 인한 코드 수정

--- client/src/components/Home/CategoryContainer.tsx
+++ client/src/components/Home/CategoryContainer.tsx
@@ -1,32 +1,15 @@
 import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 import styled from 'styled-components';
 
-import CategoryCard from '../common/CategoryCard';
 import Title from './Title';
 import CreateChannelButton from '../ui/CreateChannelButton';
+import CategoryCardList from './CategoryCardList';
 
 import { CategoryType } from '../../types/dataTypes';
-import { useNavigate } from 'react-router-dom';
-import categoryData from '../../data/categoryData';
 import { getData } from '../../api/apiCollection';
-
-const makeCategoryArray = (data: CategoryType[]) => {
-  const newCategories: CategoryType[] = [];
-
-  data.forEach((category) => {
-    const newCategory = {
-      ...category,
-      categoryName: categoryData[category.categoryName].text,
-      categoryIcon: categoryData[category.categoryName].icon
-    };
-
-    category.categoryName === 'OTHER'
-      ? newCategories.unshift(newCategory)
-      : newCategories.push(newCategory);
-  });
-
-  return newCategories;
-};
+import makeCategoryArray from '../../utils/makeCategoryArray';
 
 const CategoryContainer = () => {
   const [categories, setCategories] = useState<CategoryType[]>([]);
@@ -55,15 +38,7 @@ const CategoryContainer = () => {
           onClick={onClickCreateButtonHandler}
         />
       </StyledTitleContainer>
-      <StyledCategoryCardContainer>
-        {categories.length > 0 ? (
-          categories.map((category) => (
-            <CategoryCard key={category.categoryId} {...category} />
-          ))
-        ) : (
-          <StyledErrorMsg>카테고리가 존재하지 않습니다.</StyledErrorMsg>
-        )}
-      </StyledCategoryCardContainer>
+      <CategoryCardList categories={categories} />
     </StyledWrapper>
   );
 };
@@ -76,28 +51,10 @@ const StyledWrapper = styled.div`
   gap: 2rem;
 `;
 
-const StyledCategoryCardContainer = styled.div`
-  display: flex;
-  gap: 2rem;
-  width: 100%;
-  overflow: scroll;
-  padding: 0 1rem;
-  @media screen and (min-width: 650px) {
-    width: calc(100vw - 50px);
-  }
-`;
 const StyledTitleContainer = styled.div`
   display: flex;
   padding: 3rem 3.5rem;
   align-items: center;
   justify-content: space-between;
   width: 100%;
-`;
-
-const StyledErrorMsg = styled.div`
-  width: 100%;
-  font-size: 1.6rem;
-  padding: 1rem;
-  text-align: center;
-  color: var(--default-text-color);
 `;
