e288aaddbea0a21714fd33c316b5f51e6dd11284
crowwan <crowwan@naver.com>
2023-06-07 20:28:01 +0900

    :recycle: refactor: 상태 하위 컴포넌트로 이동
    Home에서 관리하던 상태 show를 하위 컴포넌트 NoticeModal로 이동함에 따라 코드 수정

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -2,24 +2,22 @@ import styled from 'styled-components';
 
 import CategoryContainer from '../components/Home/CategoryContainer';
 import GameContainer from '../components/Home/GameContainer';
-import { useState } from 'react';
 import NoticeModal from '../components/NoticeModal/NoticeModal';
 
 const Home = () => {
-  const [show, setShow] = useState(true);
   return (
     <>
-      {show && <NoticeModal setShow={setShow} />}
-      <StyleContain>
+      <NoticeModal />
+      <StyledContainer>
         <CategoryContainer />
         <GameContainer />
-      </StyleContain>
+      </StyledContainer>
     </>
   );
 };
 
 export default Home;
-const StyleContain = styled.div`
+const StyledContainer = styled.div`
   width: 100%;
   min-height: calc(100vh - 50px);
   background-color: var(--title-bg);
