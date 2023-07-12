11e3e4bd35b926b7fa4eb194abbbb78b09bd8e2d
crowwan <crowwan@naver.com>
2023-07-12 17:44:55 +0900

    :lable: type: changePostState 함수 파라미터 타입 추가
    BookmarkDataType, ReactionDataType, CommentDataType 추가

--- client/src/types/parameterTypes.ts
+++ client/src/types/parameterTypes.ts
@@ -1,5 +1,20 @@
+import { BookmarkType, CommentType, ReactionType } from './dataTypes';
+
 export type InputChangeType =
   | React.ChangeEvent<HTMLInputElement>
   | React.ChangeEvent<HTMLTextAreaElement>;
 
 export type SubmitType = React.FormEvent<HTMLFormElement> | React.MouseEvent;
+
+export type BookmarkDataType = {
+  bookmark: BookmarkType | null;
+};
+
+export type ReactionDataType = {
+  reaction: ReactionType | null;
+  likeCount: number;
+  unlikeCount: number;
+};
+export type CommentDataType = {
+  comments: CommentType[];
+};
